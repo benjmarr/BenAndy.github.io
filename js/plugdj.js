ac8b78c3ce42a4cb1916b35dc8e7a5318976311f
Ben Andy <warrnamboolgames@gmail.com>
2014-12-30 14:12:15 +1100

    Mhmm

--- js/plugdj.js
+++
@@ -1,6 +0,0 @@
-// Do not steal code without direct permission from it's owner
-
-API.on(API.chatLog("Share AutoPlug with your freinds! http://goo.gl/uyz0lv"));
-$("#woot").click();
-API.on(API.ADVANCE, autoplug);
-function autoplug(){ $("#woot").click(); }
