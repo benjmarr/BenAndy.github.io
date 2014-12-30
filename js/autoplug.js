84348be4437a4ce4f861a490124209af983ae14e
Ben Andy <warrnamboolgames@gmail.com>
2014-12-31 00:31:24 +1100

    idek anymore

--- js/autoplug.js
+++ js/autoplug.js
@@ -2,8 +2,9 @@
 
 API.on(API.chatLog("AutoPlug: Loading... "));
 $("#dj-button").click();
+$("div.button.cancel").click();
 $("#woot").click();
 API.on(API.ADVANCE, autoplug);
-function autoplug(){  $("#dj-button").click(); $("#woot").click(); }
-API.on(API.chatLog("AutoPlug: Alpha 0.1 "));
+function autoplug(){  $("#dj-button").click(); $("#woot").click(); $("div.button.cancel").click(); }
+API.on(API.chatLog("AutoPlug: Alpha 0.2 "));
 API.on(API.chatLog("AutoPlug: AutoPlug activated! "));
