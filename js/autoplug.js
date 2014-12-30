a5d1fb39b0c151f54b387e7ce40cdbb94454b527
Ben Andy <warrnamboolgames@gmail.com>
2014-12-30 19:47:00 +1100

    ye bby

--- js/autoplug.js
+++ js/autoplug.js
@@ -1,6 +1,6 @@
 // Do not steal code without direct permission from it's owner
 
-API.on(API.chatLog("Share AutoPlug with your freinds! "));
+API.on(API.chatLog("AutoPlug enabled! "));
 $("#woot").click();
 API.on(API.ADVANCE, autoplug);
 function autoplug(){ $("#woot").click(); }
