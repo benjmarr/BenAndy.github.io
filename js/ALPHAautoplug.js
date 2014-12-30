790d365c961c26f8983bf6ee5aa1f274dea71958
Ben Andy <warrnamboolgames@gmail.com>
2014-12-31 08:35:18 +1100

    One day, this very happy snow man went outside

--- js/ALPHAautoplug.js
+++ js/ALPHAautoplug.js
@@ -5,7 +5,7 @@ http://benandy.github.io
 DO NOT STEAL ANY CODE WITHOUT DIRECTLY GIVEN PERMISSIONS FROM IT'S OWNER
 */
 
-API.on(API.chatLog("AutoPlug| Loading AutoPlug version 0.1.6 "));
+API.on(API.chatLog("AutoPlug| Loading AutoPlug version 0.1.7 "));
 // Enter Dj Wait List
 $("#dj-button").click();
 // Cancel leaving Dj Wait List
@@ -14,10 +14,18 @@ $("div.button.cancel").click();
 $("#woot").click();
 API.on(API.ADVANCE, autoplug);
 function autoplug(){ 
+
+	// Enter Dj Wait List
 	$("#dj-button").click(); 
+	API.on(API.chatLog("AutoPlug| Joined Dj Wait List"));
+
+	// Cancel leaving Dj Wait List
 	$("div.button.cancel").click(); 
+
+	// 'Woot' the current song
 	$("#woot").click(); }
 	API.on(API.chatLog("AutoPlug| Song 'Wooted'"));
+
 API.on(API.chatLog("AutoPlug| AutoPlug loaded! "));
 API.on(API.chatLog(" "));
 API.on(API.chatLog("AutoPlug| Share AutoPlug with your freinds! "));
