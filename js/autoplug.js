60864adb001f899fa0668e69c695ef6e6114e9bc
Ben Andy <warrnamboolgames@gmail.com>
2014-12-30 23:57:14 +1100

    WHY

--- js/autoplug.js
+++ js/autoplug.js
@@ -1,8 +1,6 @@
 // Do not steal code without direct permission from it's owner
 
 API.on(API.chatLog("AutoPlug: Loading... "));
-$("#woot").click();
-$("#dj-button").click();
 API.on(API.ADVANCE, autoplug);
 function autoplug(){ $("#woot").click(); $("#dj-button").click(); }
 API.on(API.chatLog("AutoPlug: AutoPlug activated! "));
