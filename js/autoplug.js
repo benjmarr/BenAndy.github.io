522d4bbbdd863069df6c1064343cf05f619e72c7
Ben Andy <warrnamboolgames@gmail.com>
2014-12-31 03:54:49 +1100

    mkdenn

--- js/autoplug.js
+++ js/autoplug.js
@@ -1,10 +1,20 @@
-// Do not steal code without direct permission from it's owner
+/*
+Developed by Ben_Andy
+http://benandy.github.io
 
-API.on(API.chatLog("AutoPlug: Loading... "));
+DO NOT STEAL ANY CODE WITHOUT DIRECTLY GIVEN PERMISSIONS FROM IT'S OWNER
+*/
+
+API.on(API.chatLog("AutoPlug| Loading AutoPlug version 0.1.2 "));
+// Enter Dj Wait List
 $("#dj-button").click();
+// Cancel leaving Dj Wait List
 $("div.button.cancel").click();
+// 'Woot' the current song
 $("#woot").click();
 API.on(API.ADVANCE, autoplug);
-function autoplug(){  $("#dj-button").click(); $("#woot").click(); $("div.button.cancel").click(); }
-API.on(API.chatLog("AutoPlug: Alpha 0.7 "));
-API.on(API.chatLog("AutoPlug: AutoPlug activated! "));
+function autoplug(){  $("#dj-button").click(); $("div.button.cancel").click(); $("#woot").click(); }
+API.on(API.chatLog("AutoPlug| AutoPlug loaded! "));
+API.on(API.chatLog(""));
+API.on(API.chatLog("AutoPlug| Share AutoPlug with your freinds! "));
+API.on(API.chatLog("AutoPlug| http://benandy.github.io/randoms/autoplug "));
