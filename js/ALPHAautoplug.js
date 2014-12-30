2399aef2083bbdde4b3707e825efb424a20b5342
Ben Andy <warrnamboolgames@gmail.com>
2014-12-31 08:24:41 +1100

    New topic!

--- js/ALPHAautoplug.js
+++ js/ALPHAautoplug.js
@@ -5,17 +5,15 @@ http://benandy.github.io
 DO NOT STEAL ANY CODE WITHOUT DIRECTLY GIVEN PERMISSIONS FROM IT'S OWNER
 */
 
-API.on(API.chatLog("AutoPlug| Loading AutoPlug version 0.1.8 "));
+API.on(API.chatLog("AutoPlug| Loading AutoPlug version 0.1.4 "));
 // Enter Dj Wait List
 $("#dj-button").click();
 // Cancel leaving Dj Wait List
 $("div.button.cancel").click();
 // 'Woot' the current song
 $("#woot").click();
-// Advertise in chat
-$("#chat-input-field").writeln("test.Message_!=fail");
 API.on(API.ADVANCE, autoplug);
-function autoplug(){  $("#dj-button").click(); $("div.button.cancel").click(); $("#woot").click(); $("#chat-input-field").writeln("test"); }
+function autoplug(){ $("#dj-button").click(); $("div.button.cancel").click(); $("#woot").click(); }
 API.on(API.chatLog("AutoPlug| AutoPlug loaded! "));
 API.on(API.chatLog(" "));
 API.on(API.chatLog("AutoPlug| Share AutoPlug with your freinds! "));
