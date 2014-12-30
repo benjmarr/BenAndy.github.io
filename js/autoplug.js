18e13d1752edfd74dc86ff2125aab99bd30b3497
Ben Andy <warrnamboolgames@gmail.com>
2014-12-31 00:13:04 +1100

    is this a thing

--- js/autoplug.js
+++ js/autoplug.js
@@ -1,8 +1,8 @@
 // Do not steal code without direct permission from it's owner
 
 API.on(API.chatLog("AutoPlug: Loading... "));
-$("#woot").click();
 $("#dj-button").click();
+$("#woot").click();
 API.on(API.ADVANCE, autoplug);
-function autoplug(){ $("#woot").click(); $("#dj-button").click(); }
+function autoplug(){  $("#dj-button").click(); $("#woot").click(); }
 API.on(API.chatLog("AutoPlug: AutoPlug activated! "));
