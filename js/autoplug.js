ed65fddc429e2ac610d4e0623959a8f0b9276fc8
Ben Andy <warrnamboolgames@gmail.com>
2014-12-30 23:07:42 +1100

    lmao

--- js/autoplug.js
+++ js/autoplug.js
@@ -3,6 +3,6 @@
 API.on(API.chatLog("AutoPlug: Loading... "));
 $("#woot").click();
 API.on(API.ADVANCE, autoplug);
-function autoplug(){ $("#woot").click(); }
+function autoplug(){ $("#woot").click(); };
 function autoplug(){ $("#dj-button").click(); }
 API.on(API.chatLog("AutoPlug: AutoPlug activated! "));
