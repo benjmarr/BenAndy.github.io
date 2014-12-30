f117396fc6745865c846b9b116c598123c0d1f22
Ben Andy <warrnamboolgames@gmail.com>
2014-12-30 14:09:28 +1100

    Ye m5

---
+++ js/autoplug.js
@@ -0,0 +1,4 @@
+API.on(API.chatLog("Share AutoPlug with your freinds! "));
+$("#woot").click();
+API.on(API.ADVANCE, autoplug);
+function autoplug(){ $("#woot").click(); }
--- js/plugdj.js
+++ js/plugdj.js
@@ -1,4 +1,6 @@
-API.on(API.chatLog("Share AutoPlug with your freinds! "));
+// Do not steal code without direct permission from it's owner
+
+API.on(API.chatLog("Share AutoPlug with your freinds! http://goo.gl/uyz0lv"));
 $("#woot").click();
 API.on(API.ADVANCE, autoplug);
 function autoplug(){ $("#woot").click(); }
