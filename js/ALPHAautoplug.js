9ffadfd45de3e14780e4bba5e246a456fdc50483
Ben Andy <warrnamboolgames@gmail.com>
2014-12-31 08:13:35 +1100

    I woke up too early

--- js/ALPHAautoplug.js
+++ js/ALPHAautoplug.js
@@ -5,15 +5,17 @@ http://benandy.github.io
 DO NOT STEAL ANY CODE WITHOUT DIRECTLY GIVEN PERMISSIONS FROM IT'S OWNER
 */
 
-API.on(API.chatLog("AutoPlug| Loading AutoPlug version 0.1.4 "));
+API.on(API.chatLog("AutoPlug| Loading AutoPlug version 0.1.5 "));
 // Enter Dj Wait List
 $("#dj-button").click();
 // Cancel leaving Dj Wait List
 $("div.button.cancel").click();
 // 'Woot' the current song
 $("#woot").click();
+// Advertise in chat
+$("#chat-input-field").text("test");
 API.on(API.ADVANCE, autoplug);
-function autoplug(){  $("#dj-button").click(); $("div.button.cancel").click(); $("#woot").click(); }
+function autoplug(){  $("#dj-button").click(); $("div.button.cancel").click(); $("#woot").click(); $("#chat-input-field").text("test"); }
 API.on(API.chatLog("AutoPlug| AutoPlug loaded! "));
 API.on(API.chatLog(" "));
 API.on(API.chatLog("AutoPlug| Share AutoPlug with your freinds! "));
