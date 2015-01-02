7b34a33240662528b35799b61bda3baef4faca37
Ben Andy <warrnamboolgames@gmail.com>
2015-01-02 16:40:20 +1100

    AutoPlug v0.1.4.40

--- js/autoplug.js
+++ js/autoplug.js
@@ -5,13 +5,8 @@
 	DO NOT STEAL ANY CODE WITHOUT DIRECTLY GIVEN PERMISSIONS FROM IT'S OWNER
 */
 
-API.on(API.chatLog("AutoPlug| AutoPlug v0.1.3.40 "));
+API.on(API.chatLog("AutoPlug| AutoPlug v0.1.4.40 "));
 API.on(API.chatLog("AutoPlug| Developed and copyrighted (c) by Ben_Andy "));
-// Anti-AFK
-setInterval(function() {
-	API.on(API.sendChat("l"));
-	API.on(API.chatLog("AutoPlug| Reset AFK time"));
-}, 600000);
 // Enter Dj Wait List
 $("#dj-button").click();
 // Cancel leaving Dj Wait List
@@ -32,6 +27,18 @@ function autoplug(){
 	$("#woot").click();
 	API.on(API.chatLog("AutoPlug| Song 'Wooted'"));
 }
+
+// Anti-AFK
+setInterval(function() {
+	API.on(API.sendChat("l"));
+	API.on(API.chatLog("AutoPlug| Reset AFK time"));
+}, 600000);
+
+// Sharing AutoPlug
+setInterval(funtion() {
+	API.on(AWPI.chatLog("AutoPlug| Share AutoPlug with your friends!"))
+	API.on(AWPI.chatLog("AutoPlug| http://benandy.github.io/randoms/autoplug"))
+}, 9000000);
 
 API.on(API.chatLog(" "));
 API.on(API.chatLog("AutoPlug| Share AutoPlug with your freinds! "));
