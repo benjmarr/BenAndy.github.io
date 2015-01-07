52cf82f40de9f3ce730cf357272a5ced0b96a364
Ben Andy <warrnamboolgames@gmail.com>
2015-01-08 04:38:02 +1100

    blehe

--- js/vbot.js
+++ js/vbot.js
@@ -1,7 +1,29 @@
-API.on(API.chatLog("Loading VultureBot v0.1.3.8"));
-$("#chat-button").click();
+/*
+	DEVELOPED AND MANAGED BY Ben_Andy (BenAndy)
+	http://benandy.github.io
 
-API.on(API.ADVANCE, autowoot);
-function autowoot() {
-	$("#woot").click(); 
+	DO NOT USE ANY CODE FROM THIS SCRIPT WITHOUT DIRECT PERMISSIONS FROM IT'S OWNER
+*/
+
+
+API.on(API.chatLog("Loading VultureBot v0.1.4.9 "));
+API.on(API.ADVANCE, vulturebot);
+function vulturebot() {
+
+	// Woot the song
+	var autowoot = setInterval(function() {
+		$("#woot").click();
+		clearInterval(autowoot);
+	}, 2000);
+
+	// AutoPlug support
+	var autoplugSupport = setInterval(function() {
+		API.on(API.sendChat("VultureBot supports AutoPlug "));
+		API.on(API.sendChat("Click this link to use it! "));
+		API.on(API.sendChat("http://goo.gl/NlQiko "))
+	}, 900000);
+
+	if .current = > 45 {
+		API.on(API.chatLog("> 45"));
+	}
 }
