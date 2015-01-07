f0e1b3b71fe39749417693db72a6c01c2051b884
Ben Andy <warrnamboolgames@gmail.com>
2015-01-08 02:40:38 +1100

    ERMAHGAWD

--- js/vbot.js
+++ js/vbot.js
@@ -5,12 +5,17 @@
 	DO NOT STEAL ANY CODE WITHOUT DIRECTLY GIVEN PERMISSIONS FROM IT'S OWNER
 */
 
-API.on(API.chatLog("Loading VultureBot v0.0.2.3"));
+API.on(API.snedChat("Loading VultureBot v0.0.3.4"));
+API.on(API.sendChat("/cap 1"));
+API.on(API.sendChat("Capped avatars."));
 
+// When user joins the room																<-- TESTING
 API.on(API.USER_JOIN, listener); {
-
+	API.on(API.chatLog("USER_JOIN"));
 }
+//																						<-- TESTING
 
+API.on(API.sendChat("Loading plug.dj API"));
 API.on(API.ADVANCE, autoplug);
 function autoplug(){
 	// Auto woot
