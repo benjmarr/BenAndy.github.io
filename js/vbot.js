c3899d385592fc318f4d8159ae6f836bb81d6f69
Ben Andy <warrnamboolgames@gmail.com>
2015-01-08 04:17:03 +1100

    Fuuck thiiisss shiiiitt

--- js/vbot.js
+++ js/vbot.js
@@ -1,24 +1,7 @@
-/*
-	Developed by Ben_Andy (BenAndy)
-	http://benandy.github.io
+API.on(API.chatLog("Loading VultureBot v0.1.3.8"));
+$("#chat-button").click();
 
-	DO NOT STEAL ANY CODE WITHOUT DIRECTLY GIVEN PERMISSIONS FROM IT'S OWNER
-*/
-
-API.on(API.sendChat("Loading VultureBot v0.0.3.8"));
-
-API.on(API.ADVANCE, autoplug);
-function autoplug(){
-	// Auto woot
-	var autowoot = setInterval(funcion() {
-		$("#woot").click();	
-		clearInterval(autowoot);
-	}, 2000);
+API.on(API.ADVANCE, autowoot);
+function autowoot() {
+	$("#woot").click(); 
 }
-
-// AutoPlug support
-var autoplugSupport = setInterval(funcion() {
-	API.on(API.sendChat("We support #AutoPlug!"));
-	API.on(API.sendChat("Download it here:"));
-	API.on(API.sendChat("http://goo.gl/NlQiko"));
-},900000 );
