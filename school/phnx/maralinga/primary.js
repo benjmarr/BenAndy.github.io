5b9116749ec601b2b47218a4a547175c17227047
Ben Andy <warrnamboolgames@gmail.com>
2015-06-06 11:03:17 +1000

    Stuff

--- school/phnx/maralinga/primary.js
+++ school/phnx/maralinga/primary.js
@@ -1,2 +1,4 @@
 console.info("This website is under creative commons copyright, 2015.\nAny content stolen from this website can and will result in legal actions.");
 console.warn("You have been warned!");
+console.log("");
+console.info("Page information gathered from various sites and put together by Vijay Chand, Cody Quarrell and Ben Watts-Marr.");
