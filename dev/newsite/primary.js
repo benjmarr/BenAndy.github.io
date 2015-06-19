a92b603b8dbe4921603fd97adbc7d20046237e16
Ben Andy <warrnamboolgames@gmail.com>
2015-06-19 21:54:54 +1000

    Mwuhhaa

---
+++ dev/newsite/primary.js
@@ -0,0 +1,18 @@
+console.log("File '\\primary.js' loaded!")
+
+function makeHeader() {
+  document.getElementById("headerTitle").innerHTML = "Benjamin";
+  document.getElementById("headerOptions").innerHTML = "<a id='headerOptions' onclick='pageHome()'>Home</a> / <a id='headerOptions' onclick='pageAbout()'>About Me</a> / <a id='headerOptions' onclick='pageContact()'>Contact</a>";
+}
+
+function pageHome() {
+
+}
+
+function pageAbout() {
+
+}
+
+function pageContact() {
+
+}
