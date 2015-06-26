5bf993447615d1befa82e90e0d537f8a0eef02bd
Ben Andy <warrnamboolgames@gmail.com>
2015-06-26 17:07:12 +1000

    phew

---
+++ dev/1/js/main.js
@@ -0,0 +1,16 @@
+console.info("This page is under creative commons copyright.");
+
+function buildPage() {
+  document.getElementById("title").innerHTML = "Benjamin"; // Page title
+  document.getElementById("siteLinks").innerHTML = "<a href='index.html'>Home</a> / <a href='about.html'>About</a> / <a href='contact.html'>Contact</a>"; // Page links
+
+  document.getElementById("footer").innerHTML = "<div id='footerImage'><a href='http://benandy.github.io/github.html' target='_blank'><img height='30' width='30' title='GitHub!' id='githubImage' src='images/github.png' onmouseover='githubImageHover()' onmouseout='githubImageNotHover()'></a></div>Copyright &amp; Benjamin WM, 2015"; // Page footer
+}
+
+function githubImageHover() { // Change "images\github.png" to "images\githubHOVER.png" when mouse hovered
+  document.getElementById("githubImage").src = "images/githubHOVER.png";
+}
+
+function githubImageNotHover() { // Change "images\githubHOVER.png" to "images\github.png" when mouse is not hovered
+  document.getElementById("githubImage").src = "images/github.png";
+}
