0ee29ff04e0d26b92150afadb31d941a786719eb
Ben Andy <warrnamboolgames@gmail.com>
2015-06-26 22:50:05 +1000

    Yee

--- dev/1/js/main.js
+++ dev/1/js/main.js
@@ -1,9 +1,9 @@
 console.info("This page is under creative commons copyright.");
 function buildPage() {
-  document.getElementById("title").innerHTML = "<a href='inex.html'>Benjamin</a>"; // Page title
+  document.getElementById("title").innerHTML = "<a href='index.html'>Benjamin</a>"; // Page title
   document.getElementById("siteLinks").innerHTML = "<a href='index.html'>Home</a> / <a href='about.html'>About</a> / <a href='contact.html'>Contact</a>"; // Page links
 
-  document.getElementById("footer").innerHTML = "<div id='footerImage'><a href='http://benandy.github.io/github.html' target='_blank'><img height='30' width='30' title='GitHub!' id='githubImage' src='images/github.png' onmouseover='githubImageHover()' onmouseout='githubImageNotHover()'></a></div>Copyright &amp; Benjamin WM, 2015"; // Page footer
+  document.getElementById("footer").innerHTML = "<div id='footerImage'><a href='http://benandy.github.io/github.html' target='_blank'><img height='30' width='30' title='GitHub!' id='githubImage' src='images/github.png' onmouseover='githubImageHover()' onmouseout='githubImageNotHover()'></a></div>Copyright &copy; Benjamin WM, 2015"; // Page footer
 }
 
 function githubImageHover() { // Change "images\github.png" to "images\githubHOVER.png" when mouse hovered
