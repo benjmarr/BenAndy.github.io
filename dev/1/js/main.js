2df5b29d35adb4211503cecc4fb609af69320734
Ben Andy <warrnamboolgames@gmail.com>
2015-06-30 20:22:21 +1000

    Okay

--- dev/1/js/main.js
+++ dev/1/js/main.js
@@ -5,7 +5,7 @@ function buildPage() {
   // Page title tag
   document.getElementById("docTitle").innerHTML = "Benjamin";
   // Page footer
-  document.getElementById("footer").innerHTML = "<div id='footerImage'><a href='http://benandy.github.io/github.html' target='_blank'><img height='30' width='30' title='GitHub!' id='githubImage' src='images/github.png' onmouseover='githubImageHover()' onmouseout='githubImageNotHover()'></a></div>Copyright &copy; Benjamin WM, 2015";
+  document.getElementById("footer").innerHTML = "<div id='footerImage'><a href='http://benandy.github.io/github.html' target='_blank'><img height='30' width='30' title='GitHub!' id='githubImage' src='images/github.png' onmouseover='githubImageHover()' onmouseout='githubImageNotHover()'></a></div>Copyright &copy; Ben Andy, 2015";
   // Links under title
   document.getElementById("siteLinks").innerHTML = "<a href='index.html'>Home</a> / <a href='about.html'>About</a> / <a href='projects.html'>Projects</a> / <a href='contact.html'>Contact</a>";
 }
