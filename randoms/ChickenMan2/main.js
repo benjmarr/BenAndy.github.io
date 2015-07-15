de368bfb1af6ef676c521cc3e901ba40a42dc21c
Ben Andy <warrnamboolgames@gmail.com>
2015-07-15 17:51:27 +1000

    Typos!

--- randoms/ChickenMan2/main.js
+++ randoms/ChickenMan2/main.js
@@ -3,6 +3,6 @@ console.log("main.js file");
 function pageLoad() {
   console.log("Dev> function pageLoad() called");
   document.title = "ChickenMan 2.0";
-  document.getElementById("title").innerHTML = "<strong><a href='inedx.html'>ChickenMan</a></strong>";
+  document.getElementById("title").innerHTML = "<strong><a href='index.html'>ChickenMan</a></strong>";
   document.getElementById("nav").innerHTML = "<a href='index.html'>Home</a> - <a href='pic.html'>Images</a><br><br>";
 }
