8c031d8d0300d2bc062eb097fc4e2f0736f0de5b
Ben Andy <warrnamboolgames@gmail.com>
2015-07-17 21:42:17 +1000

    JavaScript file change

--- dev/1/js/404.js
+++
@@ -1,5 +0,0 @@
-console.log("This page is under creative commons copyright (c) Ben WM, 2015");
-
-var getURL = setInterval(function() {
-  document.getElementById("url404").innerHTML = "<br>" + window.location.href;
-}, 10);
---
+++ dev/1/main.js
@@ -0,0 +1,32 @@
+console.log("main.js file loaded");
+console.log("This is the main JavaScript file for the website.");
+
+function page404() {
+  console.log("page404() function called");
+  var getURL = setInterval(function() {
+    document.getElementById("url404").innerHTML = "<br>" + window.location.href;
+  }, 10);
+}
+
+function githubImageHover() { // Change "images\github.png" to "images\githubHOVER.png" when mouse hovered
+  console.log("githubImageHover() function called");
+  document.getElementById("githubImage").src = "images/githubHOVER.png";
+}
+
+function githubImageNotHover() { // Change "images\githubHOVER.png" to "images\github.png" when mouse is not hovered
+  console.log("githubImageNotHover() function claled");
+  document.getElementById("githubImage").src = "images/github.png";
+}
+
+
+function buildPage() {
+  console.log("buildPage() function called");
+  // Page title
+  document.getElementById("title").innerHTML = "<a href='index.html'>Benjamin</a>";
+  // Page title tag
+  document.title = "Error 404!";
+  // Page footer
+  document.getElementById("footer").innerHTML = "<div id='footerImage'><a href='http://benandy.github.io/github.html' target='_blank'><img height='30' width='30' title='GitHub!' id='githubImage' src='images/github.png' onmouseover='githubImageHover()' onmouseout='githubImageNotHover()'></a></div>Copyright &copy; Ben Andy, 2015";
+  // Links under title
+  document.getElementById("siteLinks").innerHTML = "<a href='index.html'>Home</a> / <a href='about.html'>About</a> / <a href='projects.html'>Projects</a> / <a href='contact.html'>Contact</a>";
+}
