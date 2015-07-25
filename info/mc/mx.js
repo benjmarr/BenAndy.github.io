83fa60234894c23a7930c8dac31067b6320118be
Ben Andy <warrnamboolgames@gmail.com>
2015-07-25 11:46:27 +1000

    yeye

---
+++ info/mc/mx.js
@@ -0,0 +1,6 @@
+console.log("mx.js file loaded\nCopyright Ben WM (c) 2015");
+
+function pageLoad() {
+  document.title = "Ben_Andy | MC";
+  document.getElementById("tableTitle").innerHTML = "MC Information | Ben_Andy";
+}
