6ed24d345ef4f7cde3ff59d2e1d662cc1b35278f
Ben Andy <warrnamboolgames@gmail.com>
2015-08-01 16:43:38 +1000

    Updates

--- randoms/madi/madi.js
+++ randoms/madi/madi.js
@@ -2,7 +2,6 @@ console.log("madi.js file loaded\nCopyright Benjamin WM (c) 2015 - All rights re
 
 function buildPage() {
   document.title = "Madison";
-  document.getElementById("copyright").innerHTML = "Copyright Benjamin &copy; 2015 - All rights reserved.";
   document.getElementById("madison").innerHTML = "Madison <a href='http://benandy.github.io/randoms/sophie' target='_self'><img onmouseover='madiHover()' onmouseout='madiNotHover()' src='content/desk.png' alt='Emoji' width='10%' height='10%'></a>";
 }
 
--- randoms/sharnie/sharnie.js
+++ randoms/sharnie/sharnie.js
@@ -2,13 +2,13 @@ console.log("madi.js file loaded\nCopyright Benjamin WM (c) 2015 - All rights re
 
 function buildPage() {
   document.title = "Sharnie";
-  document.getElementById("sharnie").innerHTML = "Sharnie <a href='' target='_self'><img onmouseover='sharnieHover()' onmouseout='sharnieNotHover()' src='content/cat.png' alt='Emoji' width='10%' height='10%'></a>";
+  document.getElementById("sharnie").innerHTML = "Sharnie <a href='' target='_self'><img onmouseover='sharnieHover()' onmouseout='sharnieNotHover()' src='content/smirk.png' alt='Emoji' width='10%' height='10%'></a>";
 }
 
 function sharnieHover() {
-  document.getElementById("sharnie").innerHTML = "Sharnie <a href='' target='_self'><img onmouseover='sharnieHover()' onmouseout='sharnieNotHover()' src='content/catDark.png' alt='Emoji' width='10%' height='10%'></a>";
+  document.getElementById("sharnie").innerHTML = "Sharnie <a href='' target='_self'><img onmouseover='sharnieHover()' onmouseout='sharnieNotHover()' src='content/smirkDark.png' alt='Emoji' width='10%' height='10%'></a>";
 }
 
 function sharnieNotHover() {
-  document.getElementById("sharnie").innerHTML = "Sharnie <a href='' target='_self'><img onmouseover='sharnieHover()' onmouseout='sharnieNotHover()' src='content/cat.png' alt='Emoji' width='10%' height='10%'></a>";
+  document.getElementById("sharnie").innerHTML = "Sharnie <a href='' target='_self'><img onmouseover='sharnieHover()' onmouseout='sharnieNotHover()' src='content/smirk.png' alt='Emoji' width='10%' height='10%'></a>";
 }
