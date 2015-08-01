b32b0fbcfc94e203165a3b6977ff1e3f62bcfd95
Ben Andy <warrnamboolgames@gmail.com>
2015-08-01 12:39:54 +1000

    Target = "_SELF"

--- randoms/madi/madi.js
+++ randoms/madi/madi.js
@@ -3,13 +3,13 @@ console.log("madi.js file loaded\nCopyright Benjamin WM (c) 2015 - All rights re
 function buildPage() {
   document.title = "Madison";
   document.getElementById("copyright").innerHTML = "Copyright Benjamin &copy; 2015 - All rights reserved.";
-  document.getElementById("madison").innerHTML = "Madison <a href='http://benandy.github.io/randoms/sophie'><img onmouseover='madiHover()' onmouseout='madiNotHover()' src='content/desk.png' alt='Emoji' width='10%' height='10%'></a>";
+  document.getElementById("madison").innerHTML = "Madison <a href='http://benandy.github.io/randoms/sophie' target='_self'><img onmouseover='madiHover()' onmouseout='madiNotHover()' src='content/desk.png' alt='Emoji' width='10%' height='10%'></a>";
 }
 
 function madiHover() {
-  document.getElementById("madison").innerHTML = "Madison <a href='http://benandy.github.io/randoms/sophie'><img onmouseover='madiHover()' onmouseout='madiNotHover()' src='content/deskDark.png' alt='Emoji' width='10%' height='10%'></a>";
+  document.getElementById("madison").innerHTML = "Madison <a href='http://benandy.github.io/randoms/sophie' target='_self'><img onmouseover='madiHover()' onmouseout='madiNotHover()' src='content/deskDark.png' alt='Emoji' width='10%' height='10%'></a>";
 }
 
 function madiNotHover() {
-  document.getElementById("madison").innerHTML = "Madison <a href='http://benandy.github.io/randoms/sophie'><img onmouseover='madiHover()' onmouseout='madiNotHover()' src='content/desk.png' alt='Emoji' width='10%' height='10%'></a>";
+  document.getElementById("madison").innerHTML = "Madison <a href='http://benandy.github.io/randoms/sophie' target='_self'><img onmouseover='madiHover()' onmouseout='madiNotHover()' src='content/desk.png' alt='Emoji' width='10%' height='10%'></a>";
 }
--- randoms/sophie/sophie.js
+++ randoms/sophie/sophie.js
@@ -3,13 +3,13 @@ console.log("madi.js file loaded\nCopyright Benjamin WM (c) 2015 - All rights re
 function buildPage() {
   document.title = "Sophie";
   document.getElementById("copyright").innerHTML = "Copyright Benjamin &copy; 2015 - All rights reserved.";
-  document.getElementById("sophie").innerHTML = "Sophie <a href='http://benandy.github.io/randoms/madi'><img onmouseover='sophHover()' onmouseout='sophNotHover()' src='content/cat.png' alt='Emoji' width='10%' height='10%'></a>";
+  document.getElementById("sophie").innerHTML = "Sophie <a href='http://benandy.github.io/randoms/madi' target='_self'><img onmouseover='sophHover()' onmouseout='sophNotHover()' src='content/cat.png' alt='Emoji' width='10%' height='10%'></a>";
 }
 
 function sophHover() {
-  document.getElementById("sophie").innerHTML = "Sophie <a href='http://benandy.github.io/randoms/madi'><img onmouseover='sophHover()' onmouseout='sophNotHover()' src='content/catDark.png' alt='Emoji' width='10%' height='10%'></a>";
+  document.getElementById("sophie").innerHTML = "Sophie <a href='http://benandy.github.io/randoms/madi' target='_self'><img onmouseover='sophHover()' onmouseout='sophNotHover()' src='content/catDark.png' alt='Emoji' width='10%' height='10%'></a>";
 }
 
 function sophNotHover() {
-  document.getElementById("sophie").innerHTML = "Sophie <a href='http://benandy.github.io/randoms/madi'><img onmouseover='sophHover()' onmouseout='sophNotHover()' src='content/cat.png' alt='Emoji' width='10%' height='10%'></a>";
+  document.getElementById("sophie").innerHTML = "Sophie <a href='http://benandy.github.io/randoms/madi' target='_self'><img onmouseover='sophHover()' onmouseout='sophNotHover()' src='content/cat.png' alt='Emoji' width='10%' height='10%'></a>";
 }
