a895a5984dcc29a63c342e6ea06c8613d2e0c946
Ben Andy <warrnamboolgames@gmail.com>
2015-08-05 22:16:02 +1000

    Numbers (Project WIP)

--- dev/1/randoms/numbers/numbers.js
+++ dev/1/randoms/numbers/numbers.js
@@ -5,9 +5,20 @@ console.log("numbers.js file loaded\nCopyright Benjamin (c) 2015 - All rights re
 var initload = setInterval(function() {
   clearInterval(initload);
   document.title = "Numbers (Use F11)";
-  document.getElementById("x").innerHTML = "<div id='sideBar'>test</div><div id='xI'>> Loading program...</div>";
+  document.getElementById("x").innerHTML = "<div id='sideBar'>filler</div><div id='xI'>> Loading program...</div>";
 
-  var programLoad = setInterval(function() {
-    clearInterval(programLoad);
-  }, 100);
+  var I = setInterval(function() {
+    clearInterval(I);
+    document.getElementById("xI").innerHTML = "> Loading program...<br><br>> Please wait!";
+
+    var II = setInterval(function() {
+      clearInterval(II);
+      document.getElementById("xI").innerHTML = "> Loading complete!";
+
+      var III = setInterval(function() {
+        clearInterval(III);
+        document.getElementById("xI").innerHTML = "> Accessing root files...<br><br>> Please wait...";
+      }, 1000);
+    }, 5000);
+  }, 2000);
 }, 10);
