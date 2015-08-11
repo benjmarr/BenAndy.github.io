6719bc580470dd36e50a3a69c2c860ac25b31a04
Ben Andy <warrnamboolgames@gmail.com>
2015-08-11 23:41:08 +1000

    Updates

--- dev/newtab/newtab.js
+++ dev/newtab/newtab.js
@@ -4,13 +4,85 @@
 
 document.title = "New Tab";
 
-document.getElementById("output").innerHTML = Math.random();
-
-var loadDelay = setInterval(function() {
-  clearInterval(loadDelay);
-  if (Math.random() < 0.5) {
-    document.body.style.background = "url('content/background/photo-1433785124354-92116416b870.jpg')";
-  } else {
-    document.body.style.background = "url('content/background/photo-1433785124354-92116416b870.jpg')";
-  }
-}, 10);
+function fadeinAnimation() {
+  var fadein1 = setInterval(function() {
+    clearInterval(fadein1);
+    document.getElementById("header").style.opacity = "0.05";
+    var fadein2 = setInterval(function() {
+      clearInterval(fadein2);
+      document.getElementById("header").style.opacity = "0.1";
+      var fadein3 = setInterval(function() {
+        clearInterval(fadein3);
+        document.getElementById("header").style.opacity = "0.15";
+        var fadein4 = setInterval(function() {
+          clearInterval(fadein4);
+          document.getElementById("header").style.opacity = "0.2";
+          var fadein5 = setInterval(function() {
+            clearInterval(fadein5);
+            document.getElementById("header").style.opacity = "0.25";
+            var fadein6 = setInterval(function() {
+              clearInterval(fadein6);
+              document.getElementById("header").style.opacity = "0.3";
+              var fadein7 = setInterval(function() {
+                clearInterval(fadein7);
+                document.getElementById("header").style.opacity = "0.35";
+                var fadein8 = setInterval(function() {
+                  clearInterval(fadein8);
+                  document.getElementById("header").style.opacity = "0.4";
+                  var fadein9 = setInterval(function() {
+                    clearInterval(fadein9);
+                    document.getElementById("header").style.opacity = "0.45";
+                    var fadein10 = setInterval(function() {
+                      clearInterval(fadein10);
+                      document.getElementById("header").style.opacity = "0.5";
+                      var fadein11 = setInterval(function() {
+                        clearInterval(fadein11);
+                        document.getElementById("header").style.opacity = "0.55";
+                        var fadein12 = setInterval(function() {
+                          clearInterval(fadein12);
+                          document.getElementById("header").style.opacity = "0.6";
+                          var fadein13 = setInterval(function() {
+                            clearInterval(fadein13);
+                            document.getElementById("header").style.opacity = "0.65";
+                            var fadein14 = setInterval(function() {
+                              clearInterval(fadein14);
+                              document.getElementById("header").style.opacity = "0.7";
+                              var fadein15 = setInterval(function() {
+                                clearInterval(fadein15);
+                                document.getElementById("header").style.opacity = "0.75";
+                                var fadein16 = setInterval(function() {
+                                  clearInterval(fadein16);
+                                  document.getElementById("header").style.opacity = "0.8";
+                                  var fadein17 = setInterval(function() {
+                                    clearInterval(fadein17);
+                                    document.getElementById("header").style.opacity = "0.85";
+                                    var fadein18 = setInterval(function() {
+                                      clearInterval(fadein18);
+                                      document.getElementById("header").style.opacity = "0.9";
+                                      var fadein19 = setInterval(function() {
+                                        clearInterval(fadein19);
+                                        document.getElementById("header").style.opacity = "0.95";
+                                        var fadein20 = setInterval(function() {
+                                          clearInterval(fadein20);
+                                          document.getElementById("header").style.opacity = "1.0";
+                                        }, 40);
+                                      }, 40);
+                                    }, 40);
+                                  }, 40);
+                                }, 40);
+                              }, 40);
+                            }, 40);
+                          }, 40);
+                        }, 40);
+                      }, 40);
+                    }, 40);
+                  }, 40);
+                }, 40);
+              }, 40);
+            }, 40);
+          }, 40);
+        }, 40);
+      }, 40);
+    }, 40);
+  }, 400);
+}
