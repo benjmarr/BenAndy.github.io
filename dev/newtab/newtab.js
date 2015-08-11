a89d5f720fd2fda652a99c85bf8390f4ff9f4e4d
Ben Andy <warrnamboolgames@gmail.com>
2015-08-11 20:03:14 +1000

    Headache

--- dev/newtab/newtab.js
+++ dev/newtab/newtab.js
@@ -4,8 +4,13 @@
 
 document.title = "New Tab";
 
-if (Math.random() < 0.5 ) {
-  document.getElementById("backgroundImage").src = "content/background/photo-1433785124354-92116416b870.jpg";
-} else {
-  document.getElementById("backgroundImage").src = "content/background/photo-1433785124354-92116416b870.jpg";
-}
+document.getElementById("output").innerHTML = Math.random();
+
+var loadDelay = setInterval(function() {
+  clearInterval(loadDelay);
+  if (Math.random() < 0.5) {
+    document.body.style.background = "url('content/background/photo-1433785124354-92116416b870.jpg')";
+  } else {
+    document.body.style.background = "url('content/background/photo-1433785124354-92116416b870.jpg')";
+  }
+}, 10);
