b19e0cf2e5cba057585ac9aa388f8cb0be947f1e
Ben Andy <warrnamboolgames@gmail.com>
2015-08-12 17:19:30 +1000

    Updates

--- dev/newtab/newtab.js
+++ dev/newtab/newtab.js
@@ -4,85 +4,36 @@
 
 document.title = "New Tab";
 
-function fadeinAnimation() {
-  var fadein1 = setInterval(function() {
-    clearInterval(fadein1);
-    document.getElementById("header").style.opacity = "0.05";
-    var fadein2 = setInterval(function() {
-      clearInterval(fadein2);
-      document.getElementById("header").style.opacity = "0.1";
-      var fadein3 = setInterval(function() {
-        clearInterval(fadein3);
-        document.getElementById("header").style.opacity = "0.15";
-        var fadein4 = setInterval(function() {
-          clearInterval(fadein4);
-          document.getElementById("header").style.opacity = "0.2";
-          var fadein5 = setInterval(function() {
-            clearInterval(fadein5);
-            document.getElementById("header").style.opacity = "0.25";
-            var fadein6 = setInterval(function() {
-              clearInterval(fadein6);
-              document.getElementById("header").style.opacity = "0.3";
-              var fadein7 = setInterval(function() {
-                clearInterval(fadein7);
-                document.getElementById("header").style.opacity = "0.35";
-                var fadein8 = setInterval(function() {
-                  clearInterval(fadein8);
-                  document.getElementById("header").style.opacity = "0.4";
-                  var fadein9 = setInterval(function() {
-                    clearInterval(fadein9);
-                    document.getElementById("header").style.opacity = "0.45";
-                    var fadein10 = setInterval(function() {
-                      clearInterval(fadein10);
-                      document.getElementById("header").style.opacity = "0.5";
-                      var fadein11 = setInterval(function() {
-                        clearInterval(fadein11);
-                        document.getElementById("header").style.opacity = "0.55";
-                        var fadein12 = setInterval(function() {
-                          clearInterval(fadein12);
-                          document.getElementById("header").style.opacity = "0.6";
-                          var fadein13 = setInterval(function() {
-                            clearInterval(fadein13);
-                            document.getElementById("header").style.opacity = "0.65";
-                            var fadein14 = setInterval(function() {
-                              clearInterval(fadein14);
-                              document.getElementById("header").style.opacity = "0.7";
-                              var fadein15 = setInterval(function() {
-                                clearInterval(fadein15);
-                                document.getElementById("header").style.opacity = "0.75";
-                                var fadein16 = setInterval(function() {
-                                  clearInterval(fadein16);
-                                  document.getElementById("header").style.opacity = "0.8";
-                                  var fadein17 = setInterval(function() {
-                                    clearInterval(fadein17);
-                                    document.getElementById("header").style.opacity = "0.85";
-                                    var fadein18 = setInterval(function() {
-                                      clearInterval(fadein18);
-                                      document.getElementById("header").style.opacity = "0.9";
-                                      var fadein19 = setInterval(function() {
-                                        clearInterval(fadein19);
-                                        document.getElementById("header").style.opacity = "0.95";
-                                        var fadein20 = setInterval(function() {
-                                          clearInterval(fadein20);
-                                          document.getElementById("header").style.opacity = "1.0";
-                                        }, 40);
-                                      }, 40);
-                                    }, 40);
-                                  }, 40);
-                                }, 40);
-                              }, 40);
-                            }, 40);
-                          }, 40);
-                        }, 40);
-                      }, 40);
-                    }, 40);
-                  }, 40);
-                }, 40);
-              }, 40);
-            }, 40);
-          }, 40);
-        }, 40);
-      }, 40);
-    }, 40);
-  }, 400);
+//photo-1438216983993-cdcd7dea84ce.jpg
+//photo-1433785124354-92116416b870.jpg
+
+if (Math.random() < 0.5) {
+  document.getElementById("header").style.backgroundImage = "url('content/background/photo-1434145175661-472d90344c15.jpg')";
+} else if (Math.random() < 0.5) {
+  document.getElementById("header").style.backgroundImage = "url('content/background/photo-1433785124354-92116416b870.jpg')";
+} else {
+  document.getElementById("header").style.backgroundImage = "url('content/background/photo-1438216983993-cdcd7dea84ce.jpg')";
+}
+
+function time() {
+  console.log("clockLoad");
+  var tday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+  var tmonth=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+  setInterval(GetClock,1000);
+  function GetClock() {
+    console.log("secondCount");
+    var d=new Date();
+    var nday=d.getDay(),nmonth=d.getMonth(),ndate=d.getDate();
+    var d=new Date();
+    var nhour=d.getHours(),nmin=d.getMinutes(),ap;
+    if(nhour==0){ap=" AM";nhour=12;}
+    else if(nhour<12){ap=" AM";}
+    else if(nhour==12){ap=" PM";}
+    else if(nhour>12){ap=" PM";nhour-=12;}
+
+    if(nmin<=9) nmin="0"+nmin;
+
+    document.getElementById('datebox').innerHTML=""+tday[nday]+", "+tmonth[nmonth]+" "+ndate+"";
+    document.getElementById('clockbox').innerHTML=""+nhour+":"+nmin+ap+"";
+  }
 }
