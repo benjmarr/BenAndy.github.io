72d0f4c0b48b7c6a11679c4f2a276797102c8c30
Ben Andy <warrnamboolgames@gmail.com>
2015-08-14 16:58:31 +1000

    Unsplash image credit
    www.unsplash.com

--- dev/newtab/newtab.js
+++ dev/newtab/newtab.js
@@ -15,25 +15,22 @@ var background = setInterval(function() {
   clearInterval(background);
 }, 100);
 
-function time() {
-  console.log("clockLoad");
-  var tday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
-  var tmonth=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
-  setInterval(GetClock,1000);
-  function GetClock() {
-    console.log("secondCount");
-    var d=new Date();
-    var nday=d.getDay(),nmonth=d.getMonth(),ndate=d.getDate();
-    var d=new Date();
-    var nhour=d.getHours(),nmin=d.getMinutes(),ap;
-    if(nhour==0){ap=" AM";nhour=12;}
-    else if(nhour<12){ap=" AM";}
-    else if(nhour==12){ap=" PM";}
-    else if(nhour>12){ap=" PM";nhour-=12;}
+console.log("clockLoad");
+var tday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+var tmonth=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+setInterval(GetClock,1000);
 
-    if(nmin<=9) nmin="0"+nmin;
-
-    document.getElementById('datebox').innerHTML=""+tday[nday]+", "+tmonth[nmonth]+" "+ndate+"";
-    document.getElementById('clockbox').innerHTML=""+nhour+":"+nmin+ap+"";
-  }
+function GetClock() {
+  console.log("secondCount");
+  var d=new Date();
+  var nday=d.getDay(),nmonth=d.getMonth(),ndate=d.getDate();
+  var d=new Date();
+  var nhour=d.getHours(),nmin=d.getMinutes(),ap;
+  if(nhour==0){ap=" AM";nhour=12;}
+  else if(nhour<12){ap=" AM";}
+  else if(nhour==12){ap=" PM";}
+  else if(nhour>12){ap=" PM";nhour-=12;}
+  if(nmin<=9) nmin="0"+nmin;
+  document.getElementById('datebox').innerHTML=""+tday[nday]+", "+tmonth[nmonth]+" "+ndate+"";
+  document.getElementById('clockbox').innerHTML=""+nhour+":"+nmin+ap+"";
 }
