99353fd1dd0fd627e20d21dfde663a607864329a
Ben Andy <warrnamboolgames@gmail.com>
2015-08-14 18:57:52 +1000

    NewTab Updates
    Updates to the NewTab page

--- dev/newtab/newtab.js
+++ dev/newtab/newtab.js
@@ -5,7 +5,35 @@
 document.title = "New Tab"
 
 function options() {
-  
+  document.getElementById("optionsMenu").style.visibility = "visible";
+  document.getElementById("optionsImg").style.visibility = "hidden";
+  document.getElementById("optionsImg").style.height = "0px";
+}
+
+function optionsClose() {
+  document.getElementById("optionsMenu").style.visibility = "hidden";
+  document.getElementById("optionsImg").style.visibility = "visible";
+  document.getElementById("optionsImg").style.height = "3%";
+}
+
+function optionsTEXTCOLOUR1() {
+  document.getElementById("optionTextColour").innerHTML = "<a onclick='optionsTEXTCOLOUR2()'>Colour Scheme</a>";
+  document.getElementById("optionsMenu").style.backgroundColor = "white";
+  document.getElementById("options").style.color = "black";
+  document.getElementById("unsplash").style.color = "black";
+  document.getElementById("clockbox").style.color = "black";
+  document.getElementById("datebox").style.color = "black";
+  document.getElementById("newtab").style.color = "black";
+}
+
+function optionsTEXTCOLOUR2() {
+  document.getElementById("optionTextColour").innerHTML = "<a onclick='optionsTEXTCOLOUR1()'>Colour Scheme</a>";
+  document.getElementById("optionsMenu").style.backgroundColor = "black";
+  document.getElementById("options").style.color = "white";
+  document.getElementById("unsplash").style.color = "white";
+  document.getElementById("clockbox").style.color = "white";
+  document.getElementById("datebox").style.color = "white";
+  document.getElementById("newtab").style.color = "white";
 }
 
 function optionsHover() {
@@ -17,17 +45,18 @@ function optionsNotHover() {
 }
 
 var background = setInterval(function() {
-  if (Math.random() < 0.5) {
+  if (Math.random() < 0.3) {
     document.getElementById("header").style.backgroundImage = "url('content/background/photo-1436891620584-47fd0e565afb.jpg')";
-  } else if (Math.random() < 0.5) {
-    document.getElementById("header").style.backgroundImage = "url('content/background/photo-1436891678271-9c672565d8f6.jpg')";
   } else {
-    document.getElementById("header").style.backgroundImage = "url('content/background/photo-1437532437759-a0ce0535dfed.jpg')";
+    if (Math.random() < 0.3) {
+      document.getElementById("header").style.backgroundImage = "url('content/background/photo-1436891678271-9c672565d8f6.jpg')";
+    } else {
+      document.getElementById("header").style.backgroundImage = "url('content/background/photo-1437532437759-a0ce0535dfed.jpg')";
+    }
   }
   clearInterval(background);
-}, 100);
+}, 1);
 
-console.log("clockLoad");
 var tday=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
 var tmonth=new Array("January","February","March","April","May","June","July","August","September","October","November","December");
 setInterval(GetClock,1000);
