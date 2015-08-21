2c858fb19f27e6d4173536aad3347bbbd6acf7dc
Ben Andy <warrnamboolgames@gmail.com>
2015-08-21 22:07:47 +1000

    AutoPlug updates

--- js/autoplugDEV.js
+++ js/autoplugDEV.js
@@ -2,8 +2,5 @@
 
 console.log("autoplug.js file loaded\nCopyirght Benjamin (c) 2015 - All rights reserved");;
 
-var autoplug = "AutoPlug >> ";
-var plug = autoplug.fontcolor("green");
-
-API.on(API.chatLog(plug + "AutoPlug v0.2.8.73"));
-API.on(API.chatLog(plug + "Copyright Benjamin (c) 2015 - All rights reserved"));
+API.on(API.chatLog("AutoPlug >> AutoPlug v0.2.8.73"));
+API.on(API.chatLog("AutoPlug >> Copyright Benjamin (c) 2015 - All rights reserved"));
