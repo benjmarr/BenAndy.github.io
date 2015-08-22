27ba7c71b3cb1435b93df4798a6189c345519b6b
Ben Andy <warrnamboolgames@gmail.com>
2015-08-22 23:40:59 +1000

    AutoPlug updates

--- js/autoplug.js
+++ js/autoplug.js
@@ -2,12 +2,14 @@
 
 console.log("autoplug.js file loaded\nCopyirght Benjamin (c) 2015 - All rights reserved.");
 
-var version = "v0.2.9.85";
+var version = "v0.2.10.87";
 
 API.on(API.chatLog("Starting AutoPlug " + version + "!\nPlease wait."));
 
 var perSecond = setInterval(function() {
-	$('#chat-input-field').attr('placeholder','Testing123');
+	$("#chat-input-field").attr("placeholder","Click here to type your chat message!");
+	$("dj-button").click();
+	$("div.button.cancel").click();
 }, 1000);
 
 var loadDelay = setInterval(function() {
