4803d372d4fafb2cbbea951d52a88b9e2f8bb636
Ben Andy <warrnamboolgames@gmail.com>
2015-08-22 23:12:25 +1000

    AutoPlug updates > v0.2.9.79

--- js/autoplug.js
+++ js/autoplug.js
@@ -1,9 +1,9 @@
 // Copyright Benjamin (c) 2015 - All rights reserved
 
-console.log("autoplug.js file loaded\nCopyirght Benjamin (c) 2015 - All rights reserved");;
+console.log("autoplug.js file loaded\nCopyirght Benjamin (c) 2015 - All rights reserved.");;
 
-API.on(API.chatLog("AutoPlug >> AutoPlug v0.2.9.78"));
-API.on(API.chatLog("AutoPlug >> Copyright Benjamin (c) 2015 - All rights reserved"));
+API.on(API.chatLog("AutoPlug >> AutoPlug v0.2.9.79"));
+API.on(API.chatLog("AutoPlug >> Copyright Benjamin (c) 2015 - All rights reserved."));
 
 var loadDelay = setInterval(function() {
 	clearInterval(loadDelay);
