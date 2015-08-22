4101d12c4156f269bcdd720cac24cbaa04ecf882
Ben Andy <warrnamboolgames@gmail.com>
2015-08-23 00:11:30 +1000

    PLAY updates

--- js/play.js
+++ js/play.js
@@ -1,5 +1,5 @@
 // Copyright Benjamin (c) 2015 - All rights reserved.
-var version = "v0.1.2.7";
+var version = "v0.1.2.8";
 var namever = "PLAY " + version;
 
 console.log("play.js file loaded\nCopyirght Benjamin (c) 2015 - All rights reserved.");
@@ -10,3 +10,4 @@ var load = setInterval(function() {
   clearInterval(load);
 
 }, 1000);
+alert(namever + " loaded!");
