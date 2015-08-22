47da1112d94d0d917e5d8758faf20422b80f2d81
Ben Andy <warrnamboolgames@gmail.com>
2015-08-23 00:26:45 +1000

    PLAY updates

--- js/play.js
+++ js/play.js
@@ -1,5 +1,5 @@
 // Copyright Benjamin (c) 2015 - All rights reserved.
-var version = "v0.1.2.13";
+var version = "v0.1.2.14";
 var namever = "PLAY " + version;
 
 console.log("play.js file loaded\nCopyirght Benjamin (c) 2015 - All rights reserved.");
@@ -8,6 +8,6 @@ console.log(namever);
 alert("Loading " + namever + "!");
 var load = setInterval(function() {
   clearInterval(load);
-  document.getElementsByTagName("body").innerHTML = "<iframe src='http://benandy.github.io/dev/PLAY/PLAY.html></iframe>'";
+  document.getElementsByTagName("body")[0].innerHTML = "<iframe src='http://benandy.github.io/dev/PLAY/PLAY.html></iframe>'";
   document.title = namever + " window";
 }, 1000);
