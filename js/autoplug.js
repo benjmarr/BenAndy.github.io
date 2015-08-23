5fd42e45ba8b41aee082a1c6f97be6b969b1cf92
Ben Andy <warrnamboolgames@gmail.com>
2015-08-23 16:53:45 +1000

    AutoPlug updates

--- js/autoplug.js
+++ js/autoplug.js
@@ -1,6 +1,6 @@
 // Copyright Benjamin (c) 2015 - All rights reserved.
 
-var version = "v0.2.12.112";
+var version = "v0.2.12.113";
 var app = "AutoPlug " + version;
 var appDetail = app + " for Plug.Dj";
 
@@ -49,6 +49,7 @@ API.on(API.WAIT_LIST_UPDATE, waitlistupdate);
 function waitlistupdate() {
 	$("#dj-button").click();
 	$("div.button.cancel").click();
+	API.on(API.sendChat("list_update"));
 }
 
 API.on(API.chatLog(app + " loaded."));
