02382c9230aadf0c1a448dc03a6d920b1356eec0
Ben Andy <warrnamboolgames@gmail.com>
2015-08-24 22:08:34 +1000

    Access

--- dev/access/access.js
+++ dev/access/access.js
@@ -9,49 +9,49 @@ var pageLoad = setInterval(function() {
 
   var daReload = setInterval(function() {
     if (Math.random() < 0.3) {
-      document.getElementById("daLIST").innerHTML = "<span class='red'>[ACCESS DENIED]</span><br /><span class='red'>[ACCESS DENIED]</span><br /><span class='lime'>[ACCESS GRANTED]</span><br /><span class='red'>[ACCESS DENIED]</span><br /><span class='lime'>[ACCESS GRANTED]</span>";
+      document.getElementById("daLIST").innerHTML = "ACCESS<br /><br /><span class='red'>[ACCESS DENIED]</span><br /><span class='red'>[ACCESS DENIED]</span><br /><span class='lime'>[ACCESS GRANTED]</span><br /><span class='red'>[ACCESS DENIED]</span><br /><span class='lime'>[ACCESS GRANTED]</span>";
     } else if (Math.random() < 0.3) {
-      document.getElementById("daLIST").innerHTML = "<span class='lime'>[ACCESS GRANTED]</span><br /><span class='lime'>[ACCESS GRANTED]</span><br /><span class='red'>[ACCESS DENIED]</span><br /><span class='lime'>[ACCESS GRANTED]</span><br /><span class='red'>[ACCESS DENIED]</span>";
+      document.getElementById("daLIST").innerHTML = "ACCESS<br /><br /><span class='lime'>[ACCESS GRANTED]</span><br /><span class='lime'>[ACCESS GRANTED]</span><br /><span class='red'>[ACCESS DENIED]</span><br /><span class='lime'>[ACCESS GRANTED]</span><br /><span class='red'>[ACCESS DENIED]</span>";
     } else if (Math.random() < 0.3) {
-      document.getElementById("daLIST").innerHTML = "<span class='red'>[ACCESS DENIED]</span><br /><span class='lime'>[ACCESS GRANTED]</span><br /><span class='lime'>[ACCESS GRANTED]</span><br /><span class='lime'>[ACCESS GRANTED]</span><br /><span class='lime'>[ACCESS GRANTED]</span>";
+      document.getElementById("daLIST").innerHTML = "ACCESS<br /><br /><span class='red'>[ACCESS DENIED]</span><br /><span class='lime'>[ACCESS GRANTED]</span><br /><span class='lime'>[ACCESS GRANTED]</span><br /><span class='lime'>[ACCESS GRANTED]</span><br /><span class='lime'>[ACCESS GRANTED]</span>";
     } else {
-      document.getElementById("daLIST").innerHTML = "<span class='lime'>[ACCESS GRANTED]</span><br /><span class='red'>[ACCESS DENIED]</span><br /><span class='lime'>[ACCESS GRANTED]</span><br /><span class='red'>[ACCESS DENIED]</span><br /><span class='red'>[ACCESS DENIED]</span>";
+      document.getElementById("daLIST").innerHTML = "ACCESS<br /><br /><span class='lime'>[ACCESS GRANTED]</span><br /><span class='red'>[ACCESS DENIED]</span><br /><span class='lime'>[ACCESS GRANTED]</span><br /><span class='red'>[ACCESS DENIED]</span><br /><span class='red'>[ACCESS DENIED]</span>";
     }
   }, 100);
   var attemptReload = setInterval(function() {
     if (Math.random() < 0.3) {
-      document.getElementById("attemptLIST").innerHTML = "> 1<br />> 2<br />> 3<br />> 4<br />> 5<br />";
+      document.getElementById("attemptLIST").innerHTML = "ID<br /><br />> 1<br />> 2<br />> 3<br />> 4<br />> 5<br />";
     } else if (Math.random() < 0.3) {
-      document.getElementById("attemptLIST").innerHTML = "> 2<br />> 3<br />> 4<br />> 5<br />> 1<br />";
+      document.getElementById("attemptLIST").innerHTML = "ID<br /><br />> 2<br />> 3<br />> 4<br />> 5<br />> 1<br />";
     } else if (Math.random() < 0.3) {
-      document.getElementById("attemptLIST").innerHTML = "> 3<br />> 4<br />> 5<br />> 1<br />> 2<br />";
+      document.getElementById("attemptLIST").innerHTML = "ID<br /><br />> 3<br />> 4<br />> 5<br />> 1<br />> 2<br />";
     } else {
-      document.getElementById("attemptLIST").innerHTML = "> 4<br />> 5<br />> 1<br />> 2<br />> 3<br />";
+      document.getElementById("attemptLIST").innerHTML = "ID<br /><br />> 4<br />> 5<br />> 1<br />> 2<br />> 3<br />";
     }
   }, 100);
   var locationReload = setInterval(function() {
     if (Math.random() < 0.3) {
-      document.getElementById("locationLIST").innerHTML = "[Australia]<br />[UNKNOWN]<br />[New_Zealand]<br />[Moscow]<br />[Washington]";
+      document.getElementById("locationLIST").innerHTML = "LOCATION<br /><br />[Australia]<br />[UNKNOWN]<br />[New_Zealand]<br />[Moscow]<br />[Washington]";
     } else if (Math.random() < 0.3) {
-      document.getElementById("locationLIST").innerHTML = "[U.S.A]<br />[Paris]<br />[New_York]<br />[Sydney]<br />[Antartica]";
+      document.getElementById("locationLIST").innerHTML = "LOCATION<br /><br />[U.S.A]<br />[Paris]<br />[New_York]<br />[Sydney]<br />[Antartica]";
     } else if (Math.random() < 0.3) {
-      document.getElementById("locationLIST").innerHTML = "[Hollywood]<br />[Mexico]<br />[UNKNOWN]<br />[California]<br />[Los_Angeleas]";
+      document.getElementById("locationLIST").innerHTML = "LOCATION<br /><br />[Hollywood]<br />[Mexico]<br />[UNKNOWN]<br />[California]<br />[Los_Angeleas]";
     } else {
-      document.getElementById("locationLIST").innerHTML = "[Russia]<br />[Canada]<br />[China]<br />[UNKNWON]<br />[Japan]";
-    }
-  }, 200);
-  var ipReload = setInterval(function() {
-    if (Math.random() < 0.3) {
-      document.getElementById("ipLIST").innerHTML = "192.168.0.120<br />281.274.5.388<br />257.950.4.358<br />749.163.7.947<br />747.185.9.146";
-    } else if (Math.random() < 0.3) {
-      document.getElementById("ipLIST").innerHTML = "281.274.5.388<br />257.950.4.358<br />749.163.7.947<br />747.185.9.146<br />192.168.0.120";
-    } else if (Math.random() < 0.3) {
-      document.getElementById("ipLIST").innerHTML = "747.185.9.146<br />192.168.0.120<br />281.274.5.388<br />257.950.4.358<br />749.163.7.947";
-    } else {
-      document.getElementById("ipLIST").innerHTML = "746.956.1.846<br />573.462.4.583<br />472.955.3.573<br />945.567.2.572<br />672.582.4.582";
+      document.getElementById("locationLIST").innerHTML = "LOCATION<br /><br />[Russia]<br />[Canada]<br />[China]<br />[UNKNWON]<br />[Japan]";
     }
   }, 200);
   var serialReload = setInterval(function() {
-    document.getElementById("serialLIST").innerHTML = Math.random() + "<br />" + Math.random() + "<br />" + Math.random() + "<br />" + Math.random() + "<br />" + Math.random();
+    document.getElementById("serialLIST").innerHTML = "SERIAL ID<br /><br />" + Math.random() + "<br />" + Math.random() + "<br />" + Math.random() + "<br />" + Math.random() + "<br />" + Math.random();
   }, 50);
+  var ipReload = setInterval(function() {
+    if (Math.random() < 0.3) {
+      document.getElementById("ipLIST").innerHTML = "IP ADDRESS<br /><br />192.168.0.120<br />281.274.5.388<br />257.950.4.358<br />749.163.7.947<br />747.185.9.146";
+    } else if (Math.random() < 0.3) {
+      document.getElementById("ipLIST").innerHTML = "IP ADDRESS<br /><br />281.274.5.388<br />257.950.4.358<br />749.163.7.947<br />747.185.9.146<br />192.168.0.120";
+    } else if (Math.random() < 0.3) {
+      document.getElementById("ipLIST").innerHTML = "IP ADDRESS<br /><br />747.185.9.146<br />192.168.0.120<br />281.274.5.388<br />257.950.4.358<br />749.163.7.947";
+    } else {
+      document.getElementById("ipLIST").innerHTML = "IP ADDRESS<br /><br />746.956.1.846<br />573.462.4.583<br />472.955.3.573<br />945.567.2.572<br />672.582.4.582";
+    }
+  }, 200);
 }, load);
