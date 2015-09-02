41143b655c49cfa5ab4bb4b0d103d79c17e14144
Ben Andy <warrnamboolgames@gmail.com>
2015-09-02 19:18:22 +1000

    AutoPlug updates

--- js/autoplug.js
+++ js/autoplug.js
@@ -1,6 +1,6 @@
 // Copyright Benjamin (c) 2015 - All rights reserved.
 
-var version = "v0.2.13.122";
+var version = "v0.2.13.123";
 var app = "AutoPlug " + version;
 var appDetail = app + " for Plug.Dj";
 var songTime = API.getTimeRemaining();
