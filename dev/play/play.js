f4ecf936c7761c819725cbc0bc3fef988244805d
Ben Andy <warrnamboolgames@gmail.com>
2015-09-09 20:15:15 +1000

    Play! updates

--- dev/play/play.js
+++ dev/play/play.js
@@ -1,13 +1,13 @@
 // Copyright Benjamin (c) 2015 - All rights reserved
 
-var version = "v0.1.3.8";
+var version = "v0.1.3.9";
 var app = "Play! " + version;
 var appDetail = app + " online game script";
 
 document.title = "Loading Play!";
 
 function options() {
-
+  alert("options == CLLICKED");
 }
 
 document.title = app;
