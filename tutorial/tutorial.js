c815d42f18222f3f4e0fa3ce41907b2b9b183459
Ben Andy <warrnamboolgames@gmail.com>
2015-09-12 15:39:50 +1000

    Web Tutorial updates

--- tutorial/tutorial.js
+++ tutorial/tutorial.js
@@ -1,3 +1,13 @@
 // Copyright Benjamin (c) 2015 - All rights reserved
 
 console.log("tutorial.js file loaded\nCopyright Benjamin (c) 2015 - All rights reserved");
+
+function headerMouseover() {
+  document.getElementById("title").style.color = "#52EFFF";
+  document.getElementById("header").style.backgroundColor = "#FF3459";
+}
+
+function headerMouseout() {
+  document.getElementById("title").style.color = "#FF3459";
+  document.getElementById("header").style.backgroundColor = "#52EFFF";
+}
