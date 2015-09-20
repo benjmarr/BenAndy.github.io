51cbedd94fbab69beeea0d9c43bda3dfb8e3ebd7
Ben Andy <warrnamboolgames@gmail.com>
2015-09-20 12:44:48 +1000

    Change Something

---
+++ dev/3/main.js
@@ -0,0 +1,20 @@
+// Copyright Benjamin (c) 2015 - All rights reserved
+
+console.log("Copyright Benjamin (c) 2015 - All rights reserved");
+document.title = "Change Something";
+
+function startHover() {
+  document.getElementById("startButton").style.backgroundColor = "#3CFF63";
+  document.getElementById("startText").style.paddingTop = "27%";
+  document.getElementById("body").style.backgroundColor = "#CCCCCC";
+}
+
+function startUnHover() {
+  document.getElementById("startButton").style.backgroundColor = "#DEDEDE";
+  document.getElementById("startText").style.paddingTop = "20%";
+  document.getElementById("body").style.backgroundColor = "#FCFCFC";
+}
+
+function startClick() {
+
+}
