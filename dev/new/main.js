1bd465f28d261e0cb94b3e7e685858cd1039f8d8
Ben Andy <warrnamboolgames@gmail.com>
2015-09-22 13:26:02 +1000

    Cowie help me pls

--- dev/new/main.js
+++ dev/new/main.js
@@ -6,4 +6,7 @@ function page404() {
   var url = window.location.href;
   console.log("Requested URL: " + url);
   document.getElementById("404x").innerHTML = "Error 404<br />Page was not found";
+  setTimeout(function() {
+    document.getElementById("404y").style.width = "500px";
+  }, 500);
 }
