78f1d6019b5321bd433c75c3aa46dc1d2fbc1dc2
Ben Andy <warrnamboolgames@gmail.com>
2015-09-22 13:35:11 +1000

    New 404

--- dev/new/main.js
+++ dev/new/main.js
@@ -7,9 +7,10 @@ function page404() {
   console.log("Requested URL: " + url);
   document.getElementById("404x").innerHTML = "Error 404<br />Page was not found";
   setTimeout(function() {
+    document.getElementById("body").style.backgroundColor = "#3A3A3A";
     document.getElementById("404y").style.width = "500px";
     setTimeout(function() {
       document.getElementById("404x").style.opacity = "1";
     }, 400);
-  }, 100);
+  }, 1);
 }
