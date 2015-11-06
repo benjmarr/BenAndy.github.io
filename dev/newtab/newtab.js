e76932e41a09ea868a3c9d460fcc4388838994e4
Benjamin <warrnamboolgames@gmail.com>
2015-11-06 21:59:26 +1100

    Skye New Tab update: Content directory move

--- dev/newtab/newtab.js
+++ dev/newtab/newtab.js
@@ -1,6 +1,6 @@
 // Copyright Benjamin (c) All rights reserved - 2015
 
-console.log("New Tab page copyright (c) Benjamin, 2015 - All rights reserved.");
+console.log("Skye New Tab page copyright (c) Benjamin, 2015 - All rights reserved.");
 var backgroundNumber = Array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44);
 var randomBackground = backgroundNumber[Math.floor(Math.random() * backgroundNumber.length)];
 var PAGEurl = "http://benandy.github.io/dev/newtab";
