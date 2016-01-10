9fc7c7121a28f3f86a42f4a7a851fefb4866b107
Benjamin <warrnamboolgames@gmail.com>
2016-01-10 21:31:53 +1100

    No longer needed

--- info/mc/mx.js
+++
@@ -1,8 +0,0 @@
-// Copyright Benjamin (c) 2015 - All rights reserved
-
-console.log("mx.js file loaded\nCopyright Ben WM (c) 2015");
-
-function pageLoad() {
-  document.title = "Ben_Andy | MC";
-  document.getElementById("tableTitle").innerHTML = "MC Information | Ben_Andy";
-}
