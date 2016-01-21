1a84c5230f8686c2f9f025214766b239bbe9b21f
Benjamin <warrnamboolgames@gmail.com>
2016-01-22 07:12:15 +1100

    Snapchat Online

---
+++ dev/snapchat/snap.js
@@ -0,0 +1,31 @@
+// Copyright Benjamin (c) 2016 - All rights reserved
+
+if (document.addEventListener) {
+  document.addEventListener("contextmenu", function(e) {
+    e.preventDefault();
+  }, false);
+} else {
+  document.attachEvent("oncontextmenu", function() {
+    window.event.returnValue = false;
+  });
+}
+
+var opacity = function(id, opacity, delay) {
+  setTimeout(function() {
+    document.getElementById(id).style.opacity = opacity;
+  }, delay);
+};
+
+setTimeout(function() {
+  document.getElementById("body").style.backgroundColor = "#FFFC00";
+}, 1000);
+
+opacity("img", "1", 2000);
+opacity("x", "1", 2500);
+opacity("log", "1", 3000);
+opacity("userPass", "1", 3500);
+
+document.getElementById("press").addEventListener("click", function() {
+  document.getElementById("log").innerHTML = "Woah woah, wait... What the hell are you doing?!";
+  document.getElementById("userPass").innerHTML = "You were just about to give your Snapchat log in details to a random website? You don't know what will happen! Giving your usernames and passwords to random unofficial websites is one of the worst things you can do these days! If you've given out your log in details for accounts to random websites, you should change your passwords right away - you don't know who could have access to your accounts now. The only official Snapchat platforms are applications on mobile devices.";
+}, false);
