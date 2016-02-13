84f3d71257585dcb6e24dfd9f080997d82993f44
Benjamin <warrnamboolgames@gmail.com>
2016-02-13 11:57:05 +1100

    Cookie policy wip

---
+++ skye/cookies.js
@@ -0,0 +1,7 @@
+// Copyright Benjamin (c) 2016 - All rights reserved
+
+document.getElementById("header").style.backgroundImage = "url('content/background/5.jpg')";
+
+setTimeout(function() {
+  document.getElementById("title").style.opacity = "1";
+}, 1500);
