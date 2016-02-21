a4ad5de66d037765bfa3678cb13dadd58d8cc434
Benjamin <warrnamboolgames@gmail.com>
2016-02-21 18:45:18 +1100

    New website updates

--- dev/new/js/main.js
+++ dev/new/js/main.js
@@ -1,5 +1,33 @@
 // Copyright Benjamin (c) 2015 - All rights reserved
 
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}''
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}''
+
+if (document.addEventListener) {
+  document.addEventListener("contextmenu", function(e) {
+    e.preventDefault();
+  }, false);
+} else {
+  document.attachEvent("oncontextmenu", function() {
+    window.event.returnValue = false;
+  });
+};
+
 var loadSpinDeg = 0;
 var loadSpin = setInterval(function() {
   if (loadSpinDeg === 361) {
@@ -26,11 +54,13 @@ window.onload = function() {
     setTimeout(function() {
       document.getElementById("titleBox").style.opacity = "1";
       document.getElementById("titleBox").style.fontSize = "500%";
+      setTimeout(function() {
+        document.getElementById("subTitleBox").style.opacity = "1";
+        document.getElementById("subTitleBox").style.fontSize = "130%";
+        setTimeout(function() {
+          document.getElementById("navBox").style.opacity = "1";
+        }, 200);
+      }, 300);
     }, 1000);
   }, 500);
-};
-
-
-var beHappy = function() {
-  throwAParty();
 };
