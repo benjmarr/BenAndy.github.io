3994f2200f328c4556da3465a356e84b2c6bdd58
Benjamin <warrnamboolgames@gmail.com>
2016-02-21 17:42:55 +1100

    Website updates

--- dev/new/js/main.js
+++ dev/new/js/main.js
@@ -9,3 +9,28 @@ var loadSpin = setInterval(function() {
   };
   document.getElementById("loadImg").style.transform = "rotate(" + loadSpinDeg + "deg)";
 }, 1);
+
+var pageSetup = function() {
+
+};
+
+window.onload = function() {
+  setTimeout(function() {
+    document.getElementById("loadImg").style.opacity = "0";
+    setTimeout(function() {
+      document.getElementById("loadImg").style.visibility = "hidden";
+      document.getElementById("loadImg").style.marginTop = "0";
+      document.getElementById("loadImg").style.height = "0";
+      clearInterval(loadSpin);
+    }, 1000);
+    setTimeout(function() {
+      document.getElementById("titleBox").style.opacity = "1";
+      document.getElementById("titleBox").style.fontSize = "500%";
+    }, 1000);
+  }, 500);
+};
+
+
+var beHappy = function() {
+  throwAParty();
+};
