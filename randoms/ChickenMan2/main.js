7f80a4f159dd828fc602186f572cecef04f2fb26
Benjamin <warrnamboolgames@gmail.com>
2016-03-14 00:20:28 +1100

    More cahnges

--- randoms/ChickenMan2/main.js
+++
@@ -1,8 +0,0 @@
-console.log("main.js file");
-
-function pageLoad() {
-  console.log("Dev> function pageLoad() called");
-  document.title = "ChickenMan 2.0";
-  document.getElementById("title").innerHTML = "<strong><a href='index.html'>ChickenMan</a></strong>";
-  document.getElementById("nav").innerHTML = "<a href='index.html'>Home</a> - <a href='pic.html'>Images</a><br><br>";
-}
