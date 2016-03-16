9b2c7119a73799aa3b0aa4e6a1dc0d89faf73dc7
Benjamin <warrnamboolgames@gmail.com>
2016-03-16 18:14:49 +1100

    Skye New Tab 2.0

--- dev/skye2/assets/background.js
+++ dev/skye2/assets/background.js
@@ -1,3 +1,66 @@
 // Copyright Benjamin (c) 2015 - All rights reserved
 
 console.log("background.js loaded.");
+
+// Random background selection script
+var backgroundNames = Array(61, 62, 63, 64, 65, 66, 67, 68, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60);
+var imageUpdateDate = "Sunday, 21<sup>st</sup> February 2016";
+var backgroundNumber = function() {
+  return backgroundNames[Math.floor(Math.random() * backgroundNames.length)];
+};
+
+// Setting background
+var setBackID;
+var setBack = function(id) {
+  if (id === "random") {
+    setBackID = backgroundNumber();
+    document.getElementById("header").style.backgroundImage = "url('assets/images/backgrounds/" + setBackID + ".jpg')";
+  };
+};
+setBack("random");
+
+// Background image data
+var imageData = function(imgUrl, imgAuth, imgAuthPro) {
+  // Add image data boxes
+};
+if (setBackID === 61) {
+  imageData("https://unsplash.com/photos/7ALI0RYyq6s", "Мартин Тасев", "https://unsplash.com/pumabg");
+} else if (setBackID === 62) {
+  imageData("https://unsplash.com/photos/bClr95glx6k", "Vashishta Jogi", "https://unsplash.com/jogi");
+} else if (setBackID === 63) {
+  imageData("https://unsplash.com/photos/_h7aBovKia4", "Philippe Wuyts", "https://unsplash.com/photos/_h7aBovKia4");
+} else if (setBackID === 64) {
+  imageData("https://unsplash.com/photos/-oWyJoSqBRM", "Alexey Topolyanskiy", "https://unsplash.com/devilcoders");
+} else if (setBackID === 65) {
+  imageData("https://unsplash.com/photos/93Ep1dhTd2s", "William Hook", "https://unsplash.com/williamtm")
+} else if (setBackID === 66) {
+  imageData("https://unsplash.com/photos/KqVHRmHVwwM", "Erez Attias", "https://unsplash.com/erezattias");
+} else if (setBackID === 67) {
+  imageData("https://unsplash.com/photos/E7q00J_8N7A", "NASA", "https://unsplash.com/nasa");
+} else if (setBackID === 68) {
+  imageData("https://unsplash.com/photos/U5rMrSI7Pn4", "Metthew Wiebe", "https://unsplash.com/matthewwiebe");
+} else if (setBackID === 49) {
+  imageData("https://unsplash.com/photos/Xwk4gkiMNGc", "Oliver & Hen Pritchard-Barrett", "https://unsplash.com/olliepb");
+} else if (setBackID === 50) {
+  imageData("https://unsplash.com/photos/2p1HOcpi14U", "Hide Obara", "https://unsplash.com/hideobara");
+} else if (setBackID === 51) {
+  imageData("https://unsplash.com/photos/sokDJl8L-fs", "Benjamin", "https://unsplash.com/ben_andy");
+} else if (setBackID === 52) {
+  imageData("https://unsplash.com/photos/4jcFu1byopQ", "Davide Ragusa", "https://unsplash.com/davideragusa");
+} else if (setBackID === 53) {
+  imageData("https://unsplash.com/photos/GYumuBnTqKc", "E+N Photographies", "https://unsplash.com/nicomiot");
+} else if (setBackID === 54) {
+  imageData("https://unsplash.com/photos/6-jTZysYY_U", "NASA", "https://unsplash.com/nasa");
+} else if (setBackID === 55) {
+  imageData("https://unsplash.com/photos/R3LcfTvcGWY", "Annie Spratt", "https://unsplash.com/fableandfolk");
+} else if (setBackID === 56) {
+  imageData("https://unsplash.com/photos/GSzD6vGIWKM", "Mickey O'neil", "https://unsplash.com/mickeyoneil");
+} else if (setBackID === 57) {
+  imageData("https://unsplash.com/photos/uAgLGG1WBd4", "Scott Webb", "https://unsplash.com/scottwebb");
+} else if (setBackID === 58) {
+  imageData("https://unsplash.com/photos/JibIPPrvITE", "Micheal Hull", "https://unsplash.com/michaelhull");
+} else if (setBackID === 59) {
+  imageData("https://unsplash.com/photos/-wWRHIUklxM", "Vladimir Kudinov", "https://unsplash.com/madbyte");
+} else if (setBackID === 60) {
+  imageData("https://unsplash.com/photos/tBtuxtLvAZs", "Matthew Wiebe", "https://unsplash.com/matthewwiebe");
+};
--- dev/skye2/assets/skye.js
+++ dev/skye2/assets/skye.js
@@ -1,4 +1,7 @@
 // Copyright Benjamin (c) 2015 - All rights reserved
+console.log("Skye New Tab copyright Benjamin (c) 2015 - All rights reserved");
+console.log("skye.js loaded.");
+var rootUrl = "https://github.com/BenAndy/SkyeNewTab/tree/master";
 
 // Cookie handlers
 function setCookie(cname, cvalue, exdays) {
@@ -7,11 +10,10 @@ function setCookie(cname, cvalue, exdays) {
   var expires = "expires=" + d.toUTCString();
   document.cookie = cname + "=" + cvalue + "; " + expires;
 };
-
 function getCookie(cname) {
   var name = cname + "=";
   var ca = document.cookie.split(';');
-  var (var i=0; i < ca.length; i++) {
+  for (var i = 0; i < ca.length; i++) {
     var c = ca[i];
     while (c.charAt(0) == ' ') c = c.substring(1);
     if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
@@ -28,4 +30,43 @@ if (document.addEventListener) {
   document.attachEvent("oncontextmenu", function() {
     window.event.returnValue = false;
   });
+};
+
+// Style methods
+  // Opacity style
+var styleOpacity = function(delay, id, value) {
+  setTimeout(function() {
+    document.getElementById(id).style.opacity = value;
+  }, delay);
+};
+  // Grayscale style
+var styleGrayscale = function(delay, id, value) {
+  setTimeout(function() {
+    if (value) {
+      document.getElementById(id).style.webkitFilter = "grayscale(1)";
+    } else {
+      document.getElementById(id).style.webkitFilter = "grayscale(0)";
+    };
+  }, delay);
+};
+
+// Get name
+var retriveUserName = function() {
+  var promptName = prompt("What's your name? If you don't wish to have your name shown, just press enter.", "Type your name here!");
+  if (promptName === "Type your name here!" || promptName === "" || promptName === "null") {
+    alert("You can add your name in the options menu at any time!\nJust press 'O' for options!");
+    setCookie("usingName", "false", 365);
+  } else {
+    alert("Thanks, " + promptName + ". You can change your name in the options menu at any time!\nJust press 'O' for options!");
+    setCookie("userName", promptName, 365);
+    setCookie("usingName", "true", 365);
+  };
+};
+if (getCookie("usingName") === "") {
+  retriveUserName();
+};
+
+// Run when page finished loading
+window.onload = function() {
+  styleOpacity(0, "body", "1");
 };
