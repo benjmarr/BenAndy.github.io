0c4aa3ff210f1818b8fba2d51985cb2d5ff71dc6
Benjamin <warrnamboolgames@gmail.com>
2016-03-17 17:32:47 +1100

    Skye New Tab 2.0

--- dev/skye2/assets/skye.js
+++ dev/skye2/assets/skye.js
@@ -73,12 +73,18 @@ if (getCookie("usingName") === "") {
 };
 
 // Set date, time, date alerts & welcome messages
+var nday;
+var nmonth;
+var ndate;
+var strdndth;
 var tday = new Array ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
 var tmonth = new Array ("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
 setInterval(clock, 100);
 function clock() {
   var d = new Date();
-  var nday = d.getDay(), nmonth = d.getMonth(), ndate = d.getDate();
+  nday = d.getDay()
+  nmonth = d.getMonth()
+  ndate = d.getDate();
   var nhour = d.getHours(), nmin = d.getMinutes(), ap;
   var thour = d.getHours();
   if (nhour === 0) {
@@ -96,13 +102,13 @@ function clock() {
     nmin = "0" + nmin;
   };
   if (ndate === 1 || ndate === 21 || ndate === 31) {
-    var strdndth = "st";
+    strdndth = "st";
   } else if (ndate === 2 || ndate === 22) {
-    var strdndth = "nd";
+    strdndth = "nd";
   } else if (ndate === 3 || ndate === 23) {
-    var strdndth = "rd";
+    strdndth = "rd";
   } else {
-    var strdndth = "th";
+    strdndth = "th";
   };
   if (tmonth[nmonth] === "January") {
     if (ndate === 1) {
@@ -138,20 +144,32 @@ function clock() {
   } else {
     document.getElementById("clockBox").innerHTML = nhour + ":" + nmin;
   };
-  document.getElementById("dateBox").innerHTML = tday[nday] + ", " + tmonth[nmonth] + " " + ndate + "<sup>" + strdndth + "</sup>";
+  if (getCookie("opDteFom") === "1") {
+    document.getElementById("dateBox").innerHTML = tday[nday] + ", " + tmonth[nmonth] + " " + ndate + "<sup>" + strdndth + "</sup>";
+  } else if (getCookie("opDteFom") === "2") {
+    document.getElementById("dateBox").innerHTML = tmonth[nmonth] + ndate + "<sup>" + strdndth + "</sup>";
+  } else if (getCookie("opDteFom") === "3") {
+    document.getElementById("dateBox").innerHTML = tday[nday] + ", " + ndate + " " + tmonth[nmonth];
+  } else if (getCookie("opDteFom") === "4") {
+    document.getElementById("dateBox").innerHTML = tmonth[nmonth] + " " + ndate + "<sup>" + strdndth + "</sup>";
+  };
 };
 
 // Options button click
 document.getElementById("options").addEventListener("click", function() {
   styleOpacity(0, "opM", 0.9);
   styleOpacity(0, "options", 0);
+  styleVisibility(0, "opM", "visible");
   styleVisibility(1000, "options", "hidden");
+  styleOpacity(0, "dteFom", 0);
+  styleVisibility(1000, "dteFom", "hidden");
 }, false);
 
 // Options close
 document.getElementById("opMclose").addEventListener("click", function() {
   styleOpacity(0, "opM", 0);
   styleOpacity(1000, "options", 1);
+  styleVisibility(1000, "opM", "hidden");
   styleVisibility(0, "options", "visible");
 }, false);
 
@@ -185,7 +203,7 @@ document.getElementById("opAMPM").addEventListener("click", function() {
 opAMPM("pageLoad");
   // Change background image
 document.getElementById("opChBkImg").addEventListener("click", function() {
-  styleOpacity(0, "header", "0");
+  styleOpacity(0, "header", 0);
   styleOpacity(1100, "header", "1");
   setTimeout(function() {
     setBack("random");
@@ -195,15 +213,49 @@ document.getElementById("opChBkImg").addEventListener("click", function() {
 document.getElementById("opChngNme").addEventListener("click", function() {
   retriveUserName();
 }, false);
+  // Change the date format
+document.getElementById("opChngeDteFrmt").addEventListener("click", function() {
+  styleOpacity(0, "opM", 0);
+  styleVisibility(1000, "opM", "hidden");
+  styleOpacity(1000, "dteFom", 0.9);
+  styleVisibility(500, "dteFom", "visible");
+  styleVisibility(500, "options", "visible");
+  styleOpacity(500, "options", "1");
+}, false);
+document.getElementById("dteFomclose").addEventListener("click", function() {
+  styleOpacity(0, "dteFom", 0);
+  styleVisibility(1000, "dteFom", 0);
+}, false);
+if (getCookie("opDteFom") === "") {
+  setCookie("opDteFom", 1, 365);
+};
+setTimeout(function() {
+  document.getElementById("opDteFom1").innerHTML = tday[nday] + ", " + tmonth[nmonth] + " " + ndate + "<sup>" + strdndth + "</sup>";
+  document.getElementById("opDteFom2").innerHTML = tmonth[nmonth] + " " + ndate + "<sup>" + strdndth + "</sup>";
+  document.getElementById("opDteFom3").innerHTML = tday[nday] + ", " + ndate + " " + tmonth[nmonth];
+  document.getElementById("opDteFom4").innerHTML = tmonth[nmonth] + " " + ndate + "<sup>" + strdndth + "</sup>";
+}, 200);
+document.getElementById("opDteFom1").addEventListener("click", function() {
+  setCookie("opDteFom", 1, 365);
+}, false);
+document.getElementById("opDteFom2").addEventListener("click", function() {
+  setCookie("opDteFom", 2, 365);
+}, false);
+document.getElementById("opDteFom3").addEventListener("click", function() {
+  setCookie("opDteFom", 3, 365);
+}, false);
+document.getElementById("opDteFom4").addEventListener("click", function() {
+  setCookie("opDteFom", 4, 365);
+}, false);
 
 // Run when page finished loading
 window.onload = function() {
-  styleOpacity(300, "body", "1");
-  styleOpacity(1300, "options", "1");
-  styleOpacity(1800, "unsplash", "1");
-  styleOpacity(2300, "title", "1");
-  styleOpacity(1500, "clockBox", "1");
-  styleOpacity(1900, "dateBox", "1");
-  styleOpacity(2300, "alertBox", "1");
-  styleOpacity(2700, "linkDiv", "1");
+  styleOpacity(300, "body", 1);
+  styleOpacity(1300, "options", 1);
+  styleOpacity(1800, "unsplash", 1);
+  styleOpacity(2300, "title", 1);
+  styleOpacity(1500, "clockBox", 1);
+  styleOpacity(1900, "dateBox", 1);
+  styleOpacity(2300, "alertBox", 1);
+  styleOpacity(2700, "linkDiv", 1);
 };
