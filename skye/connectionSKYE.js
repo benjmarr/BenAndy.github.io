d1b51bc0ea8209fcc2218cb88ece36ec9ee9b4fb
Benjamin <benjaminandyy@gmail.com>
2016-06-24 15:44:02 +1000

    No longer needed

--- skye/connectionSKYE.js
+++
@@ -1,8 +0,0 @@
-// Copyright Benjamin Developments (c) 2016 - All rights reserved
-
-document.getElementById("unsplash").innerHTML = "Connected";
-styleOpacity(2000, "unsplash", "0");
-setTimeout(function() {
-  document.getElementById("unsplash").innerHTML = "Images provided by <a href='https://unsplash.com' target='_blank'>unsplash.com</a>";
-}, 2001);
-styleOpacity(2002, "unsplash", "1");
