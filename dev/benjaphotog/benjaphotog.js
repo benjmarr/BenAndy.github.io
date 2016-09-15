fc8f521e0b2f4930795c39c67070c5b42e4936c0
Benjamin <benjaminandyy@gmail.com>
2016-09-15 20:24:08 +1000

    Benjamin Photography v1.0.1 Beta 2

---
+++ dev/benjaphotog/benjaphotog.js
@@ -0,0 +1,27 @@
+// Copyright Benjamin Developments (c) 2016 - All rights reserved
+
+var appVersion = "1.0.1 Beta 2";
+console.log("Benjamin Photography version " + appVersion);
+
+// Navigation bar
+if (mainIndex) {
+  // My Photos
+  document.getElementById("navOptionMyPhotos").addEventListener("click", function() {
+    // my photos page redirect
+  }, false);
+
+  // Unsplash
+    document.getElementById("navOptionUnsplash").addEventListener("click", function() {
+      // unsplash
+    }, false);
+
+  // Contact
+  document.getElementById("navOptionContact").addEventListener("click", function() {
+    // contact redirect
+  }, false);
+
+  // Facebook
+  document.getElementById("navOptionFacebook").addEventListener("click", function() {
+    // facebook redirect
+  }, false);
+};
--- dev/benjaphotog/calls.js
+++ dev/benjaphotog/calls.js
@@ -1,3 +1,4 @@
 // Copyright Benjamin Developments (c) 2016 - All rights reserved
 
-var version = "0.0.0 Beta 1";
+// Page identifiers
+var mainIndex;
