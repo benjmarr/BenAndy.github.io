2cd9d64961f859d8d9204a3adfdb180ef3295c79
Benjamin <benjaminandyy@gmail.com>
2016-09-16 18:34:18 +1000

    Benjamin Photography v1.0.1 Beta 3

--- dev/benjaphotog/benjaphotog.js
+++ dev/benjaphotog/benjaphotog.js
@@ -1,7 +1,41 @@
 // Copyright Benjamin Developments (c) 2016 - All rights reserved
 
-var appVersion = "1.0.1 Beta 2";
+var appVersion = "1.0.1 Beta 3";
 console.log("Benjamin Photography version " + appVersion);
+
+// Loading animations
+var loadSpin = function() {
+  var loadSpinDeg = 0;
+  var loadSpin = setInterval(function() {
+    if (loadSpinDeg === 361) {
+      loadSpinDeg = 0;
+    } else {
+      loadSpinDeg++;
+    };
+    document.getElementById("loadingImage").style.transform = "rotate(" + loadSpinDeg + "deg)";
+  }, 1);
+};
+if (loadSpin) {
+  loadSpin();
+};
+
+// Page loading complete
+window.onload = function() {
+  setTimeout(function() {
+    document.getElementById("loadingImage").style.opacity = "0";
+    setTimeout(function() {
+      document.getElementById("backgroundImage").style.opacity = "0.4";
+      document.getElementById("loadingImage").style.visibility = "hidden";
+      document.getElementById("loadingImage").style.marginTop = "0";
+      document.getElementById("loadingImage").style.height = "0";
+      clearInterval(loadSpin);
+      setTimeout(function() {
+        document.getElementById("title").style.opacity = "1";
+        document.getElementById("navigationBar").style.opacity = "1";
+      }, 300);
+    }, 500);
+  }, 100);
+};
 
 // Navigation bar
 if (mainIndex) {
@@ -12,7 +46,7 @@ if (mainIndex) {
 
   // Unsplash
     document.getElementById("navOptionUnsplash").addEventListener("click", function() {
-      // unsplash
+      window.open("https://unsplash.com/@ben_andy", "_blank");
     }, false);
 
   // Contact
@@ -22,6 +56,6 @@ if (mainIndex) {
 
   // Facebook
   document.getElementById("navOptionFacebook").addEventListener("click", function() {
-    // facebook redirect
+    window.open("https://www.facebook.com/BenjaPhotog/", "_blank");
   }, false);
 };
