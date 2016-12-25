d107e646c0f9c5ac5b0580006f2ec8638f61c129
BenAndy <benjaminandyy@gmail.com>
2016-12-26 08:33:41 +1100

    Minimalist Tab

---
+++ minimalisttab/main.js
@@ -0,0 +1,17 @@
+// Copyright Benjamin Developments (c) 2016-2017 - All rights reserved
+
+window.onload = function() {
+  document.getElementById("body").style.backgroundColor = "#BDBDBD";
+  setTimeout(function() {
+    document.getElementById("1").style.opacity = "1";
+    setTimeout(function() {
+      document.getElementById("2").style.opacity = "1";
+      setTimeout(function() {
+        document.getElementById("3").style.opacity = "1";
+        setTimeout(function() {
+          document.getElementById("3").style.fontSize = "200%";
+        }, 200);
+      }, 300);
+    }, 300);
+  }, 200);
+}
