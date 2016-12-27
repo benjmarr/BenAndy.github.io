b3653c08e18d6b7f84016e7fa2dd2e3703597184
BenAndy <benjaminandyy@gmail.com>
2016-12-28 03:19:27 +1100

    How Long? updates

--- randoms/howlong/howlong.js
+++ randoms/howlong/howlong.js
@@ -1,14 +1,11 @@
-// Copyright Benjamin (c) 2015 - All rights reserved
-
-console.log("Copyright Benjamin (c) 2015 - All rights reserved");
-
+// Copyright Benjamin (c) 2017 - All rights reserved
+console.log("Copyright Benjamin (c) 2017 - All rights reserved");
 function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+d.toUTCString();
     document.cookie = cname + "=" + cvalue + "; " + expires;
-}
-
+};
 function getCookie(cname) {
     var name = cname + "=";
     var ca = document.cookie.split(';');
@@ -18,31 +15,18 @@ function getCookie(cname) {
         if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
     }
     return "";
-}
-
+};
 var sec = 0;
-
+if (getCookie("record") === "") {
+  setCookie("record", 0, 365);
+  document.getElementById("record").innerHTML = getCookie("record");
+};
 setInterval(function() {
   sec = sec + 1;
   document.getElementById("time").innerHTML = sec;
+  if (sec > getCookie("record")) {
+    setCookie("record", sec, 365);
+  };
+  document.getElementById("record").innerHTML = getCookie("record");
 }, 1000);
-
-setInterval(function() {
-  if (getCookie("record") == "") {
-    setCookie("record", 0, 365);
-  } else {
-    if (sec > getCookie("record")) {
-      setCookie("record", sec, 365);
-      document.getElementById("record").innerHTML = sec;
-    } else {
-      document.getElementById("record").innerHTML = getCookie("record");
-    };
-  };
-}, 500);
-
-document.addEventListener("keydown", function(e) {
-  e = e || window.event;
-  if (e.keyCode == "82") {
-    setCookie("record", 0, 365);
-  };
-}, false);
+document.getElementById("record").innerHTML = getCookie("record");
