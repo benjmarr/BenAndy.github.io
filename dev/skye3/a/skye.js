a37c552f1c8492f109d750649a2ec88bc0335678
Benjamin <benjaminandyy@gmail.com>
2017-02-18 20:21:33 +1100

    Skye New Tab 3.0

--- dev/skye3/a/skye.js
+++ dev/skye3/a/skye.js
@@ -75,9 +75,9 @@ if (sbi === bn[0]) {
 };
 
 // Set/Read/Change/Open options
-  // Close all menus
+    // Close all menus
 var cn = function(x) {
-  var ms = Array("mom");
+  var ms = Array("mom", "dfm");
   var y = function(z) {
     setTimeout(function() {
       document.getElementById(z).style.visibility = "hidden";
@@ -102,15 +102,15 @@ var cn = function(x) {
     document.getElementById("o").style.visibility = "visible";
   };
 };
-  // Close menu button
+    // Close menu button
 document.getElementById("cn").addEventListener("click", function() {
   cn();
 }, false);
-  // Open options menu
+    // Open options menu
 document.getElementById("o").addEventListener("click", function() {
   cn(true);
 }, false);
-  // Toggle background grayscale
+    // Toggle background grayscale
 if (gc("ogs") === "") {
   sc("ogs", "f", 99*99);
 };
@@ -123,13 +123,52 @@ document.getElementById("gs").addEventListener("click", function() {
     document.getElementById("b").style.webkitFilter = "grayscale(0)";
   };
 }, false);
-  // Toggle 24 hour time
+    // Toggle 24 hour time
 if (gc("otht") === "") {
   sc("ogs", "12", 99*99);
 };
 document.getElementById("tht").addEventListener("click", function() {
-
+  if (gc("otht") === "12") {
+    gc("otht", "24", 99*99);
+  } else {
+    get("otht", "12", 99*99);
+  };
 }, false);
+    // Toggle AM/PM visibility
+if (gc("oap") === "") {
+  sc("oap", "v", 99*99);
+};
+document.getElementById("ap").addEventListener("click", function() {
+  if (gc("oap") === "v") {
+    sc("oap", "h", 99*99);
+  } else {
+    sc("oap", "v", 99*99);
+  };
+}, false);
+    // Change date format
+if (gc("dfo") === "") {
+  sc("dfo", "0", 99*99);
+};
+document.getElementById("df").addEventListener("click", function() {
+  cn();
+  setTimeout(function() {
+    document.getElementById("dfm").style.visibility = "visible";
+    document.getElementById("dfm").style.opacity = 1;
+  }, 300);
+}, false);
+document.getElementById("dfo0").addEventListener("click", function() {
+  sc("dfo", "0", 99*99);
+}, false);
+document.getElementById("dfo1").addEventListener("click", function() {
+  sc("dfo", "1", 99*99);
+}, false);
+document.getElementById("dfo2").addEventListener("click", function() {
+  sc("dfo", "2", 99*99);
+}, false);
+document.getElementById("dfo3").addEventListener("click", function() {
+  sc("dfo", "3", 99*99);
+}, false);
+// TODO MAKE SURE THIS WORKS AND FIX grayscale
 
 // Clock and date
 var nd;
@@ -145,7 +184,7 @@ function c() {
   nd = d.getDay();
   nm = d.getMonth();
   ne = d.getDate();
-  var nh = d.getHours, nmm = d.getMinutes, ap;
+  var nh = d.getHours(), nmm = d.getMinutes(), ap;
   var th = d.getHours();
   if (nh === 0) {
     ap = " AM";
@@ -192,6 +231,25 @@ function c() {
       document.getElementById("ab").innerHTML = "Good evening";
     };
   };
+  if (gc("otht") === "24") {
+    htf = nh + 12;
+    document.getElementById("cb").innerHTML = htf + ":" + nmm;
+  } else {
+    if (gc("oap") === "v") {
+      document.getElementById("cb").innerHTML = nh + ":" + nmm + ap;
+    } else {
+      document.getElementById("cb").innerHTML = mh + ":" + nmm;
+    };
+  };
+  if (gc("dfo") === "0") {
+    document.getElementById("db").innerHTML = td[nd] + ", " + tm[nm] + " " + ne + "<sup>" + st + "</sup>";
+  } else if (gc("dfo") === "1") {
+    document.getElementById("db").innerHTML = tm[nm] + " " + ne + "<sup>" + st + "</sup>";
+  } else if (gc("dfo") === "2") {
+    document.getElementById("db").innerHTML = td[nd] + ", " + ne + " " + tm[nm];
+  } else if (gc("dfo") === "3") {
+    document.getElementById("db").innerHTML = tm[nm] + " " + ne + "<sup>" + st + "</sup>";
+  };
 };
 
 // Page load complete
@@ -203,6 +261,9 @@ window.onload = function() {
   setTimeout(function() {
     document.getElementById("o").style.opacity = 1;
     document.getElementById("dt").style.opacity = 1;
+    document.getElementById("cb").style.opacity = 1;
+    document.getElementById("db").style.opacity = 1;
+    document.getElementById("ab").style.opacity = 1;
   }, 300);
   setTimeout(function() {
     document.getElementById("dt").style.opacity = 0;
