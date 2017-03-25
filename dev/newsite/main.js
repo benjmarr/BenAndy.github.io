461f652df79d528543efc98af6bc20c5719a0dbf
Benjamin <benjaminandyy@gmail.com>
2017-03-25 15:32:34 +1100

    New website

---
+++ dev/newsite/main.js
@@ -0,0 +1,39 @@
+// Copyright Benjamin Developments (c) 2016-2017 - All rights reserved
+
+// Cookie handlers
+function sc(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + "; " + expires;
+};
+function gc(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1);
+    if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+  };
+  return "";
+};
+
+// Window on load
+window.onload = function() {
+  setTimeout(function() {
+    $("header").css({"opacity": "0.2"});
+  }, 100);
+  $(".title").css({"margin-top": "0px"});
+  setTimeout(function() {
+    $(".subTitle").css({"margin-left": "0px"});
+  }, 250);
+};
+
+// Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-89454896-1', 'auto');
+ga('send', 'pageview');
